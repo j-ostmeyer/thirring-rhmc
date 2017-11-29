2194e3d37ef2cda20a0c8270512dfd82c41cb0b0
Ed Bennett <e.j.bennett@swansea.ac.uk>
2017-11-29 14:59:00 +0000

    tidy up subfolder implementation

--- submit.sh
+++ submit.sh
@@ -15,13 +15,23 @@
 #SBATCH --mem-per-cpu=1024
 
 # Specify the number of cpu cores your job requires
-#SBATCH --ntasks=1
+#SBATCH --ntasks=24
 
 # Set up the environment
 module load intel
 
 # Run the application
-mkdir "`cat compile_flags`"
-{ time ./bulk_rhmc; } 2> "`cat compile_flags`/timings"
+export OMP_NUM_THREADS=$SLURM_NTASKS
 
-mv output control fort.11 "`cat compile_flags`"
+if [ "$[OMP_NUM_THREADS]" -gt "1" ]
+then
+    FOLDER="`cat compile_flags` ${OMP_NUM_THREADS} threads"
+else
+    FOLDER="`cat compile_flags`"
+fi
+
+mkdir "$FOLDER"
+cd "$FOLDER"
+cp ../con ../midout ../remez* .
+{ time ../bulk_rhmc; } 2> timings
+rm con midout remez*
