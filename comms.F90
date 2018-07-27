af12eae98b15e205abe40179870cf224cc638683
Michele Mesiti <michele.mesiti@swansea.ac.uk>
2018-07-27 14:05:07 +0100

    Missing some ierr in comms.F90

--- comms.F90
+++ comms.F90
@@ -261,7 +261,7 @@ contains
 
 !RECEIVES
 ! Start receive in x direction
-    call MPI_Cart_Shift(comm, 0, 1, ip_xdn, ip_xup)
+    call MPI_Cart_Shift(comm, 0, 1, ip_xdn, ip_xup,ierr)
     call MPI_Irecv(Array, 1, halo_4_xdn_recv(size4), ip_xdn, 0 + tag_offset, comm, &
          & reqs(2),ierr)
     call MPI_Irecv(Array, 1, halo_4_xup_recv(size4), ip_xup, 1 + tag_offset, comm, &
@@ -276,14 +276,14 @@ contains
     call MPI_Isend(Array, 1, halo_4_ydn_send(size4), ip_ydn, 3 + tag_offset, comm, &
          & reqs(7),ierr)
 ! Start receive in y direction
-    call MPI_Cart_Shift(comm, 1, 1, ip_ydn, ip_yup)
+    call MPI_Cart_Shift(comm, 1, 1, ip_ydn, ip_yup,ierr)
     call MPI_Irecv(Array, 1, halo_4_ydn_recv(size4), ip_ydn, 2 + tag_offset, comm, &
          & reqs(6),ierr)
     call MPI_Irecv(Array, 1, halo_4_yup_recv(size4), ip_yup, 3 + tag_offset, comm, &
          & reqs(8),ierr)
 
 ! Start receive in t direction
-    call MPI_Cart_Shift(comm, 2, 1, ip_tdn, ip_tup)
+    call MPI_Cart_Shift(comm, 2, 1, ip_tdn, ip_tup,ierr)
     call MPI_Irecv(Array, 1, halo_4_tdn_recv(size4), ip_tdn, 4 + tag_offset, comm, &
          & reqs(10),ierr)
     call MPI_Irecv(Array, 1, halo_4_tup_recv(size4), ip_tup, 5 + tag_offset, comm, &
@@ -292,21 +292,21 @@ contains
 
 !SENDS
 ! Start send in x direction
-    call MPI_Cart_Shift(comm, 0, 1, ip_xdn, ip_xup)
+    call MPI_Cart_Shift(comm, 0, 1, ip_xdn, ip_xup,ierr)
     call MPI_Isend(Array, 1, halo_4_xup_send(size4), ip_xup, 0 + tag_offset, comm, &
          & reqs(1),ierr)
     call MPI_Isend(Array, 1, halo_4_xdn_send(size4), ip_xdn, 1 + tag_offset, comm, &
          & reqs(3),ierr)
 
 ! Start send in y direction
-    call MPI_Cart_Shift(comm, 1, 1, ip_ydn, ip_yup)
+    call MPI_Cart_Shift(comm, 1, 1, ip_ydn, ip_yup,ierr)
     call MPI_Isend(Array, 1, halo_4_yup_send(size4), ip_yup, 2 + tag_offset, comm, &
          & reqs(5),ierr)
     call MPI_Isend(Array, 1, halo_4_ydn_send(size4), ip_ydn, 3 + tag_offset, comm, &
          & reqs(7),ierr)
 
 ! Start send in t direction
-    call MPI_Cart_Shift(comm, 2, 1, ip_tdn, ip_tup)
+    call MPI_Cart_Shift(comm, 2, 1, ip_tdn, ip_tup,ierr)
     call MPI_Isend(Array, 1, halo_4_tup_send(size4), ip_tup, 4 + tag_offset, comm, &
          & reqs(9),ierr)
     call MPI_Isend(Array, 1, halo_4_tdn_send(size4), ip_tdn, 5 + tag_offset, comm, &
@@ -327,21 +327,21 @@ contains
 
     ! RECEIVES
     ! Start receive in x direction
-    call MPI_Cart_Shift(comm, 0, 1, ip_xdn, ip_xup)
+    call MPI_Cart_Shift(comm, 0, 1, ip_xdn, ip_xup,ierr)
     call MPI_Irecv(Array, 1, halo_4_real_xdn_recv(size4), ip_xdn, 0 + tag_offset, comm, &
          & reqs(2),ierr)
     call MPI_Irecv(Array, 1, halo_4_real_xup_recv(size4), ip_xup, 1 + tag_offset, comm, &
          & reqs(4),ierr)
 
     ! Start receive in y direction
-    call MPI_Cart_Shift(comm, 1, 1, ip_ydn, ip_yup)
+    call MPI_Cart_Shift(comm, 1, 1, ip_ydn, ip_yup,ierr)
     call MPI_Irecv(Array, 1, halo_4_real_ydn_recv(size4), ip_ydn, 2 + tag_offset, comm, &
          & reqs(6),ierr)
     call MPI_Irecv(Array, 1, halo_4_real_yup_recv(size4), ip_yup, 3 + tag_offset, comm, &
          & reqs(8),ierr)
 
     ! Start receive in t direction
-    call MPI_Cart_Shift(comm, 2, 1, ip_tdn, ip_tup)
+    call MPI_Cart_Shift(comm, 2, 1, ip_tdn, ip_tup,ierr)
     call MPI_Irecv(Array, 1, halo_4_real_tdn_recv(size4), ip_tdn, 4 + tag_offset, comm, &
          & reqs(10),ierr)
     call MPI_Irecv(Array, 1, halo_4_real_tup_recv(size4), ip_tup, 5 + tag_offset, comm, &
@@ -349,21 +349,21 @@ contains
 
     ! SENDS
     ! Start send in x direction
-    call MPI_Cart_Shift(comm, 0, 1, ip_xdn, ip_xup)
+    call MPI_Cart_Shift(comm, 0, 1, ip_xdn, ip_xup,ierr)
     call MPI_Isend(Array, 1, halo_4_real_xup_send(size4), ip_xup, 0 + tag_offset, comm, &
          & reqs(1),ierr)
     call MPI_Isend(Array, 1, halo_4_real_xdn_send(size4), ip_xdn, 1 + tag_offset, comm, &
          & reqs(3),ierr)
 
     ! Start send in y direction
-    call MPI_Cart_Shift(comm, 1, 1, ip_ydn, ip_yup)
+    call MPI_Cart_Shift(comm, 1, 1, ip_ydn, ip_yup,ierr)
     call MPI_Isend(Array, 1, halo_4_real_yup_send(size4), ip_yup, 2 + tag_offset, comm, &
          & reqs(5),ierr)
     call MPI_Isend(Array, 1, halo_4_real_ydn_send(size4), ip_ydn, 3 + tag_offset, comm, &
          & reqs(7),ierr)
 
     ! Start send in t direction
-    call MPI_Cart_Shift(comm, 2, 1, ip_tdn, ip_tup)
+    call MPI_Cart_Shift(comm, 2, 1, ip_tdn, ip_tup,ierr)
     call MPI_Isend(Array, 1, halo_4_real_tup_send(size4), ip_tup, 4 + tag_offset, comm, &
          & reqs(9),ierr)
     call MPI_Isend(Array, 1, halo_4_real_tdn_send(size4), ip_tdn, 5 + tag_offset, comm, &
@@ -385,21 +385,21 @@ contains
    
     ! RECEIVE
     ! Start receive in x direction
-    call MPI_Cart_Shift(comm, 0, 1, ip_xdn, ip_xup)
+    call MPI_Cart_Shift(comm, 0, 1, ip_xdn, ip_xup,ierr)
     call MPI_Irecv(Array, 1, halo_5_xdn_recv(size5), ip_xdn, 0 + tag_offset, comm, &
          & reqs(2),ierr)
     call MPI_Irecv(Array, 1, halo_5_xup_recv(size5), ip_xup, 1 + tag_offset, comm, &
          & reqs(4),ierr)
 
     ! Start receive in y direction
-    call MPI_Cart_Shift(comm, 1, 1, ip_ydn, ip_yup)
+    call MPI_Cart_Shift(comm, 1, 1, ip_ydn, ip_yup,ierr)
     call MPI_Irecv(Array, 1, halo_5_ydn_recv(size5), ip_ydn, 2 + tag_offset, comm, &
          & reqs(6),ierr)
     call MPI_Irecv(Array, 1, halo_5_yup_recv(size5), ip_yup, 3 + tag_offset, comm, &
          & reqs(8),ierr)
 
     ! Start receive in t direction
-    call MPI_Cart_Shift(comm, 2, 1, ip_tdn, ip_tup)
+    call MPI_Cart_Shift(comm, 2, 1, ip_tdn, ip_tup,ierr)
     call MPI_Irecv(Array, 1, halo_5_tdn_recv(size5), ip_tdn, 4 + tag_offset, comm, &
          & reqs(10),ierr)
     call MPI_Irecv(Array, 1, halo_5_tup_recv(size5), ip_tup, 5 + tag_offset, comm, &
@@ -407,21 +407,21 @@ contains
 
     !SENDS
     ! Start send in x direction
-    call MPI_Cart_Shift(comm, 0, 1, ip_xdn, ip_xup)
+    call MPI_Cart_Shift(comm, 0, 1, ip_xdn, ip_xup,ierr)
     call MPI_Isend(Array, 1, halo_5_xup_send(size5), ip_xup, 0 + tag_offset, comm, &
          & reqs(1),ierr)
     call MPI_Isend(Array, 1, halo_5_xdn_send(size5), ip_xdn, 1 + tag_offset, comm, &
          & reqs(3),ierr)
 
     ! Start send in y direction
-    call MPI_Cart_Shift(comm, 1, 1, ip_ydn, ip_yup)
+    call MPI_Cart_Shift(comm, 1, 1, ip_ydn, ip_yup,ierr)
     call MPI_Isend(Array, 1, halo_5_yup_send(size5), ip_yup, 2 + tag_offset, comm, &
          & reqs(5),ierr)
     call MPI_Isend(Array, 1, halo_5_ydn_send(size5), ip_ydn, 3 + tag_offset, comm, &
          & reqs(7),ierr)
 
     ! Start send in t direction
-    call MPI_Cart_Shift(comm, 2, 1, ip_tdn, ip_tup)
+    call MPI_Cart_Shift(comm, 2, 1, ip_tdn, ip_tup,ierr)
     call MPI_Isend(Array, 1, halo_5_tup_send(size5), ip_tup, 4 + tag_offset, comm, &
          & reqs(9),ierr)
     call MPI_Isend(Array, 1, halo_5_tdn_send(size5), ip_tdn, 5 + tag_offset, comm, &
@@ -443,21 +443,21 @@ contains
  
     ! RECEIVES
     ! Start send and receive in x direction
-    call MPI_Cart_Shift(comm, 0, 1, ip_xdn, ip_xup)
+    call MPI_Cart_Shift(comm, 0, 1, ip_xdn, ip_xup,ierr)
     call MPI_Irecv(Array, 1, halo_6_xdn_recv(size5, size6), ip_xdn, 0 + tag_offset, comm, &
          & reqs(2),ierr)
     call MPI_Irecv(Array, 1, halo_6_xup_recv(size5, size6), ip_xup, 1 + tag_offset, comm, &
          & reqs(4),ierr)
 
     ! Start send and receive in y direction
-    call MPI_Cart_Shift(comm, 1, 1, ip_ydn, ip_yup)
+    call MPI_Cart_Shift(comm, 1, 1, ip_ydn, ip_yup,ierr)
     call MPI_Irecv(Array, 1, halo_6_ydn_recv(size5, size6), ip_ydn, 2 + tag_offset, comm, &
          & reqs(6),ierr)
     call MPI_Irecv(Array, 1, halo_6_yup_recv(size5, size6), ip_yup, 3 + tag_offset, comm, &
          & reqs(8),ierr)
 
     ! Start send and receive in t direction
-    call MPI_Cart_Shift(comm, 2, 1, ip_tdn, ip_tup)
+    call MPI_Cart_Shift(comm, 2, 1, ip_tdn, ip_tup,ierr)
     call MPI_Irecv(Array, 1, halo_6_tdn_recv(size5, size6), ip_tdn, 4 + tag_offset, comm, &
          & reqs(10),ierr)
     call MPI_Irecv(Array, 1, halo_6_tup_recv(size5, size6), ip_tup, 5 + tag_offset, comm, &
@@ -465,21 +465,21 @@ contains
 
     ! SENDS
     ! Start send and receive in x direction
-    call MPI_Cart_Shift(comm, 0, 1, ip_xdn, ip_xup)
+    call MPI_Cart_Shift(comm, 0, 1, ip_xdn, ip_xup,ierr)
     call MPI_Isend(Array, 1, halo_6_xup_send(size5, size6), ip_xup, 0 + tag_offset, comm, &
          & reqs(1),ierr)
     call MPI_Isend(Array, 1, halo_6_xdn_send(size5, size6), ip_xdn, 1 + tag_offset, comm, &
          & reqs(3),ierr)
 
     ! Start send and receive in y direction
-    call MPI_Cart_Shift(comm, 1, 1, ip_ydn, ip_yup)
+    call MPI_Cart_Shift(comm, 1, 1, ip_ydn, ip_yup,ierr)
     call MPI_Isend(Array, 1, halo_6_yup_send(size5, size6), ip_yup, 2 + tag_offset, comm, &
          & reqs(5),ierr)
     call MPI_Isend(Array, 1, halo_6_ydn_send(size5, size6), ip_ydn, 3 + tag_offset, comm, &
          & reqs(7),ierr)
 
     ! Start send and receive in t direction
-    call MPI_Cart_Shift(comm, 2, 1, ip_tdn, ip_tup)
+    call MPI_Cart_Shift(comm, 2, 1, ip_tdn, ip_tup,ierr)
     call MPI_Isend(Array, 1, halo_6_tup_send(size5, size6), ip_tup, 4 + tag_offset, comm, &
          & reqs(9),ierr)
     call MPI_Isend(Array, 1, halo_6_tdn_send(size5, size6), ip_tdn, 5 + tag_offset, comm, &
