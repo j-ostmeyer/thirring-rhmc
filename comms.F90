8d5560ab56e0f5a5960e8663879ee7805d97b079
Makis Kappas <makis.kappas@gmail.com>
2020-01-14 11:17:10 +0000

    Changed to 4D cartesian communicator and enlarged requests array

--- comms.F90
+++ comms.F90
@@ -2,8 +2,11 @@ module comms
   use comms_common
   use params
   use mpi
+
   implicit none
+
 contains
+
   subroutine init_halo_types()
     use comms5, only: init_halo_types_5
     use comms5_sp, only: init_halo_types_5_sp
@@ -21,9 +24,10 @@ contains
   end subroutine init_halo_types
 
   subroutine complete_halo_update(reqs)
-    integer, intent(inout) :: reqs(12)
+    integer, intent(inout) :: reqs(16)
     integer :: ierr
-    call MPI_Waitall(12, reqs, MPI_Statuses_Ignore, ierr)
+
+    call MPI_Waitall(16, reqs, MPI_Statuses_Ignore, ierr)
   end subroutine complete_halo_update
 
   !***********************************************************************
@@ -43,35 +47,39 @@ contains
     ! Check that we have the right number of processes
     call MPI_comm_size(MPI_COMM_WORLD, np_global, ierr)
     call MPI_comm_rank(MPI_COMM_WORLD, ip_global, ierr)
-    if (np_global .ne. NP_X*NP_Y*NP_T + must_rank) then
-      if(ip_global.eq.0) then
-        print *, "MPI dimensionality mismatch: ", NP_X, "*", NP_Y, "*", NP_T, "!=", np_global
+
+    if (np_global .ne. NP_X*NP_Y*NP_T*NP_THIRD + must_rank) then
+      if (ip_global .eq. 0) then
+        print *, "MPI dimensionality mismatch: ", NP_X, "*", NP_Y, "*", NP_T, "*", NP_THIRD, "!=", np_global
       end if
       call MPI_finalize(ierr)
       call exit(2)
     end if
-    if(ip_global.eq.0) then
-      print *, "Initialising MPI with grid ", NP_X, "*", NP_Y, "*", NP_T
+
+    if (ip_global .eq. 0) then
+      print *, "Initialising MPI with grid (NP_X * NP_Y * NP_T * NP_THIRD)", NP_X, "*", NP_Y, "*", NP_T, "*", NP_THIRD
     end if
- 
+
     ! Set up a Cartesian communicator; periodic boundaries, allow reordering
-    call MPI_cart_create(MPI_COMM_WORLD, 3, (/NP_X, NP_Y, NP_T/), &
-      & (/.true., .true., .true./), .true., comm, ierr)
+    call MPI_cart_create(MPI_COMM_WORLD, 4, (/ NP_X, NP_Y, NP_T, NP_THIRD /), &
+                         (/ .true., .true., .true., .true. /), .true., comm, ierr)
 
     ! Know where I am
-    call MPI_cart_coords(comm, ip_global, 3, coords, ierr)
+    call MPI_cart_coords(comm, ip_global, 4, coords, ierr)
     ip_x = coords(1)
     ip_y = coords(2)
     ip_t = coords(3)
+    ip_third = coords(4)
 
     ! Prepare file format for MPI-IO
     call MPI_Type_Create_Subarray(4, &! dimensionality
-                                  (/ksize, ksize, ksizet, 3/), &! global volume
-                                  (/ksizex_l, ksizey_l, ksizet_l, 3/), &! local volume
-                                  (/ip_x*ksizex_l, ip_y*ksizey_l, ip_t*ksizet_l, 0/), &! start location
+                                  (/ ksize, ksize, ksizet, 3 /), &! global volume
+                                  (/ ksizex_l, ksizey_l, ksizet_l, 3 /), &! local volume
+                                  (/ ip_x*ksizex_l, ip_y*ksizey_l, ip_t*ksizet_l, 0 /), &! start location
                                   MPI_Order_Fortran, &! array ordering
                                   MPI_Real, &! datatype to store
-                                  mpiio_type, ierr) ! type descriptor for this subarray type
+                                  mpiio_type, &! type descriptor for this subarray type
+                                  ierr)
     call MPI_Type_Commit(mpiio_type, ierr)
 
     ! Prepare all of the halo types
--- comms_common.F90
+++ comms_common.F90
@@ -1,7 +1,7 @@
 module comms_common
   implicit none
   save
-  integer :: ip_x, ip_y, ip_t, ip_global, np_global
+  integer :: ip_x, ip_y, ip_t, ip_third, ip_global, np_global
   integer :: comm
   integer :: mpiio_type
 end module comms_common
