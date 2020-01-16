36a4e5e18d6a49e04efdd3046a4d1abf9f717954
Makis Kappas <makis.kappas@gmail.com>
2020-01-16 13:32:25 +0000

    Added "third" parameters for the test suite

--- tests/test_params.F90
+++ tests/test_params.F90
@@ -8,22 +8,23 @@ module params
   integer, parameter :: sp = kind(1.)
 
   ! Lattice parameters
-#define KSIZE 12
-#define KSIZET 12
+#define KSIZE 6
+#define KSIZET 6
+#define KTHIRD 8
   integer, parameter :: ksize = KSIZE, ksizet = KSIZET
-  integer, parameter :: kthird = 24
+  integer, parameter :: kthird = KTHIRD
   integer, parameter :: kvol = ksize*ksize*ksizet
   integer, parameter :: ndiag = 25, ndiagg = 12
   integer, parameter :: Nf = 1
   real(dp), parameter :: akappa = 0.5d0
 #ifndef MPI
-  integer, parameter :: ksizex_l = ksize, ksizey_l = ksize, ksizet_l = ksizet
+  integer, parameter :: ksizex_l = ksize, ksizey_l = ksize, ksizet_l = ksizet, kthird_l = kthird
   integer, parameter :: kvol_l = kvol
-  integer, parameter :: np_x = 1, np_y = 1, np_t = 1, np_global = 1
-  integer, parameter :: ip_x = 0, ip_y = 0, ip_t = 0, ip_global = 0
+  integer, parameter :: np_x = 1, np_y = 1, np_t = 1, np_third = 1, np_global = 1
+  integer, parameter :: ip_x = 0, ip_y = 0, ip_t = 0, ip_third = 0, ip_global = 0
 #else
-#if !(defined(NP_X) && defined(NP_Y) && defined(NP_T))
-#error "NP_X, NP_Y, and NP_T must be defined for MPI compilation."
+#if !(defined(NP_X) && defined(NP_Y) && defined(NP_T) && defined(NP_THIRD))
+#error "NP_X, NP_Y, NP_T and NP_THIRD must be defined for MPI compilation."
 #endif
 #if (KSIZE / NP_X) * NP_X != KSIZE
 #error "ksize must be divisible by NP_X"
@@ -31,11 +32,14 @@ module params
 #error "ksize must be divisible by NP_Y"
 #elif (KSIZET / NP_T) * NP_T != KSIZET
 #error "ksizet must be divisible by NP_T"
+#elif (KTHIRD / NP_THIRD) * NP_THIRD != KTHIRD
+#error "kthird must be divisible by NP_THIRD"
 #endif
-  integer, parameter :: np_x = NP_X, np_y = NP_Y, np_t = NP_T
+  integer, parameter :: np_x = NP_X, np_y = NP_Y, np_t = NP_T, np_third = NP_THIRD
   integer, parameter :: ksizex_l = ksize/np_x
   integer, parameter :: ksizey_l = ksize/np_y
   integer, parameter :: ksizet_l = ksizet/np_t
+  integer, parameter :: kthird_l = kthird/np_third
 #endif
 
   ! Control parameters
