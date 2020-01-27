32f2d02fb1758a2e2fd571a3f90d76ef29ff012d
Makis Kappas <makis.kappas@gmail.com>
2020-01-27 16:54:50 +0000

    Modified derivs and its unit test to work with the mpi along third dim

--- derivs_module.F90
+++ derivs_module.F90
@@ -1,4 +1,6 @@
 module derivs_module
+  use mpi
+  use comms_common, only: comm_grp_third
   implicit none
 
 contains
@@ -6,20 +8,21 @@ contains
   subroutine derivs(R, X2, anum, iflag)
     use gforce, only: dSdpi
     use dirac, only: kdelta, gamval, gamin
-    use params, only: kthird, ksizet_l, ksizey_l, ksizex_l, akappa, dp
+    use params, only: kthird_l, ksizet_l, ksizey_l, ksizex_l, akappa, dp
     !      complex, intent(in) :: R(kthird, 0:ksizex_l+1, 0:ksizey_l+1, 0:ksizet_l+1, 4)
     !      complex, intent(in) :: X2(kthird, 0:ksizex_l+1, 0:ksizey_l+1, 0:ksizet_l+1, 4)
 
-    complex(dp), intent(in) :: R(kthird, 0:ksizex_l + 1, 0:ksizey_l + 1, 0:ksizet_l + 1, 4)
-    complex(dp), intent(in) :: X2(kthird, 0:ksizex_l + 1, 0:ksizey_l + 1, 0:ksizet_l + 1, 4)
+    complex(dp), intent(in) :: R(0:kthird_l + 1, 0:ksizex_l + 1, 0:ksizey_l + 1, 0:ksizet_l + 1, 4)
+    complex(dp), intent(in) :: X2(0:kthird_l + 1, 0:ksizex_l + 1, 0:ksizey_l + 1, 0:ksizet_l + 1, 4)
     real(dp), intent(in) :: anum
     integer, intent(in) :: iflag
+    integer :: ierr
 
     !      complex(dp) :: tzi
     real :: tzi_real
     integer :: ix, iy, it, ixup, iyup, itup, idirac, mu
     integer :: igork1
-    !
+
     !     write(6,111)
     !111 format(' Hi from derivs')
 
@@ -28,7 +31,7 @@ contains
     !      tzi=cmplx(0.0,2*anum)
     tzi_real = 2*real(anum)
     !     factor of 2 picks up second term in M&M (7.215)
-    !
+
     do mu = 1, 3
       ixup = kdelta(1, mu)
       iyup = kdelta(2, mu)
@@ -38,48 +41,60 @@ contains
         do it = 1, ksizet_l
           do iy = 1, ksizey_l
             do ix = 1, ksizex_l
-              dSdpi(ix, iy, it, mu) = &
-               &     dSdpi(ix, iy, it, mu) + tzi_real*real(akappa)*sum(aimag( &
-                &       conjg(R(:, ix, iy, it, idirac))* &
-                &         X2(:, ix + ixup, iy + iyup, it + itup, idirac)) &
-          &     - aimag(conjg(R(:, ix + ixup, iy + iyup, it + itup, idirac))* &
-                &         X2(:, ix, iy, it, idirac)))
+              dSdpi(ix, iy, it, mu) = dSdpi(ix, iy, it, mu) &
+                    + tzi_real*real(akappa) &
+                    * sum(aimag(conjg(R(1:kthird_l, ix, iy, it, idirac)) &
+                              * X2(1:kthird_l, ix + ixup, iy + iyup, it + itup, idirac)) &
+                        - aimag(conjg(R(1:kthird_l, ix + ixup, iy + iyup, it + itup, idirac)) &
+                              * X2(1:kthird_l, ix, iy, it, idirac)))
             enddo
           enddo
         enddo
-        !
+
         igork1 = gamin(mu, idirac)
+
         if (iflag .eq. 0) then
+
           do it = 1, ksizet_l
             do iy = 1, ksizey_l
               do ix = 1, ksizex_l
-                dSdpi(ix, iy, it, mu) = &
-         &     dSdpi(ix, iy, it, mu) + tzi_real*sum(aimag(gamval(mu, idirac)* &
-                  &(conjg(R(:, ix, iy, it, idirac))* &
-                  &        X2(:, ix + ixup, iy + iyup, it + itup, igork1) &
-                  &+ conjg(R(:, ix + ixup, iy + iyup, it + itup, idirac))* &
-                  &             X2(:, ix, iy, it, igork1))))
+                dSdpi(ix, iy, it, mu) = dSdpi(ix, iy, it, mu) &
+                        + tzi_real &
+                        * sum(aimag(gamval(mu, idirac) &
+                            * (conjg(R(1:kthird_l, ix, iy, it, idirac)) &
+                              * X2(1:kthird_l, ix + ixup, iy + iyup, it + itup, igork1) &
+                             + conjg(R(1:kthird_l, ix + ixup, iy + iyup, it + itup, idirac)) &
+                              * X2(1:kthird_l, ix, iy, it, igork1))))
               enddo
             enddo
           enddo
+
         else
+
           do it = 1, ksizet_l
             do iy = 1, ksizey_l
               do ix = 1, ksizex_l
-                dSdpi(ix, iy, it, mu) = &
-         &     dSdpi(ix, iy, it, mu) - tzi_real*sum(aimag(gamval(mu, idirac)* &
-                  &(conjg(R(:, ix, iy, it, idirac))* &
-                  &        X2(:, ix + ixup, iy + iyup, it + itup, igork1) &
-                  &+ conjg(R(:, ix + ixup, iy + iyup, it + itup, idirac))* &
-                  &             X2(:, ix, iy, it, igork1))))
+                dSdpi(ix, iy, it, mu) = dSdpi(ix, iy, it, mu) &
+                        - tzi_real &
+                        * sum(aimag(gamval(mu, idirac) &
+                            * (conjg(R(1:kthird_l, ix, iy, it, idirac)) &
+                              * X2(1:kthird_l, ix + ixup, iy + iyup, it + itup, igork1) &
+                             + conjg(R(1:kthird_l, ix + ixup, iy + iyup, it + itup, idirac)) &
+                              * X2(1:kthird_l, ix, iy, it, igork1))))
               enddo
             enddo
           enddo
+
         endif
-        !
+
       enddo
     enddo
-    !
+
+#ifdef MPI
+    call MPI_AllReduce(MPI_IN_PLACE, dSdpi, ksizex_l*ksizey_l*ksizet_l*3, &
+                       MPI_DOUBLE_PRECISION, MPI_Sum, comm_grp_third, ierr)
+#endif
+
     return
   end subroutine derivs
 
--- tests/test_derivs.F90
+++ tests/test_derivs.F90
@@ -1,6 +1,6 @@
 #include "test_utils.fh"
 program test_derivs
-  use dwf3d_lib
+  ! use dwf3d_lib
   use dirac
   use gforce
   use comms
@@ -20,10 +20,10 @@ program test_derivs
 
   ! initialise function parameters
   complex(dp) u(0:ksizex_l + 1, 0:ksizey_l + 1, 0:ksizet_l + 1, 3)
-  complex(dp) Phi(kthird, 0:ksizex_l + 1, 0:ksizey_l + 1, 0:ksizet_l + 1, 4)
-  complex(dp) X2(kthird, 0:ksizex_l + 1, 0:ksizey_l + 1, 0:ksizet_l + 1, 4)
+  complex(dp) Phi(0:kthird_l + 1, 0:ksizex_l + 1, 0:ksizey_l + 1, 0:ksizet_l + 1, 4)
+  complex(dp) X2(0:kthird_l + 1, 0:ksizex_l + 1, 0:ksizey_l + 1, 0:ksizet_l + 1, 4)
   real :: dSdpi_ref(ksizex_l, ksizey_l, ksizet_l, 3)
-  complex(dp) R(kthird, 0:ksizex_l + 1, 0:ksizey_l + 1, 0:ksizet_l + 1, 4)
+  complex(dp) R(0:kthird_l + 1, 0:ksizex_l + 1, 0:ksizey_l + 1, 0:ksizet_l + 1, 4)
   real :: diff(ksizex_l, ksizey_l, ksizet_l, 3), sum_diff, max_diff
 
   real(dp), parameter :: anum = tau
@@ -32,20 +32,24 @@ program test_derivs
   integer :: i, j, ix, iy, it, ithird
   integer, parameter :: idxmax = 4*ksize*ksize*ksizet*kthird
   integer :: idx = 0
+
 #ifdef MPI
-  integer, dimension(12) :: reqs_R, reqs_Phi, reqs_X2
+  integer, dimension(16) :: reqs_R, reqs_Phi, reqs_X2
   integer :: ierr
   call init_MPI
 #endif
+
   do j = 1, 4
     do it = 1, ksizet_l
       do iy = 1, ksizey_l
         do ix = 1, ksizex_l
-          do ithird = 1, kthird
-            idx = ithird + (ip_x*ksizex_l + ix - 1)*kthird &
-              & + (ip_y*ksizey_l + iy - 1)*kthird*ksize &
-              & + (ip_t*ksizet_l + it - 1)*kthird*ksize*ksize &
-              & + (j - 1)*kthird*ksize*ksize*ksizet
+          do ithird = 1, kthird_l
+            idx = ip_third*kthird_l + ithird &
+                + (ip_x*ksizex_l + ix - 1)*kthird &
+                + (ip_y*ksizey_l + iy - 1)*kthird*ksize &
+                + (ip_t*ksizet_l + it - 1)*kthird*ksize*ksize &
+                + (j - 1)*kthird*ksize*ksize*ksizet
+
             Phi(ithird, ix, iy, it, j) = 1.1*exp(iunit*idx*tau/idxmax)
             R(ithird, ix, iy, it, j) = 1.3*exp(iunit*idx*tau/idxmax)
             X2(ithird, ix, iy, it, j) = 0.5*exp(1.0)*exp(iunit*idx*tau/idxmax)
@@ -54,25 +58,29 @@ program test_derivs
       enddo
     enddo
   enddo
+
 #ifdef MPI
   call start_halo_update_5(4, R, 0, reqs_R)
   call start_halo_update_5(4, Phi, 0, reqs_Phi)
   call start_halo_update_5(4, X2, 0, reqs_X2)
 #endif
+
   do j = 1, 3
     do it = 1, ksizet_l
       do iy = 1, ksizey_l
         do ix = 1, ksizex_l
           idx = ip_x*ksizex_l + ix &
-            & + (ip_y*ksizey_l + iy - 1)*ksize &
-            & + (ip_t*ksizet_l + it - 1)*ksize*ksize &
-            & + (j - 1)*ksize*ksize*ksizet
+              + (ip_y*ksizey_l + iy - 1)*ksize &
+              + (ip_t*ksizet_l + it - 1)*ksize*ksize &
+              + (j - 1)*ksize*ksize*ksizet
+
           u(ix, iy, it, j) = exp(iunit*idx*tau/idxmax)
           dSdpi_ref(ix, iy, it, j) = real(tau*exp(iunit*idx*tau/idxmax), sp)
         enddo
       enddo
     enddo
   enddo
+
 #ifdef MPI
   call complete_halo_update(reqs_R)
   call complete_halo_update(reqs_Phi)
@@ -82,6 +90,7 @@ program test_derivs
   call update_halo_5(4, R)
   call update_halo_4(3, u)
 #endif
+
   ! initialise common variables
   beta = 0.4
   am3 = 1.0
@@ -90,24 +99,36 @@ program test_derivs
   call init_gammas()
   ! call function
   do i = 1, timing_loops
-    dSdpi = dSdpi_ref
+    ! dSdpi = dSdpi_ref ! Commented out for the mpi third version
     call derivs(R, X2, anum, iflag)
+    ! dSdpi_ref should be added after calling derivs subroutine
+    ! otherwise the reference value is added more than once in each group
+    ! due to the MPI_AllReduce operation inside derivs
+    dSdpi = dSdpi + dSdpi_ref
   end do
+
   ! check output
   if (generate) then
+    print *, "Generating .dat file..."
     write_file(dSdpi, 'test_derivs.dat', MPI_Real)
   else
     read_file(dSdpi_ref, 'test_derivs.dat', MPI_Real)
 
+    ! diff will now have duplicates for the same (x,y,t,mu)
+    ! due to the parallelization along third dimension
+    ! and the MPI_AllReduce operation. For this the sum_diff
+    ! will be larger than expected (np_third times larger).
+    ! Because of that we divide by np_third
     diff = dSdpi - dSdpi_ref
-    sum_diff = sum(diff)
+    sum_diff = sum(diff)/np_third
     max_diff = maxval(abs(diff))
 #ifdef MPI
     call MPI_AllReduce(MPI_IN_PLACE, sum_diff, 1, MPI_Real, MPI_Sum, &
-      & comm, ierr)
+                       comm, ierr)
     call MPI_AllReduce(MPI_IN_PLACE, max_diff, 1, MPI_Real, MPI_Max, &
-      & comm, ierr)
+                       comm, ierr)
 #endif
+
     if (ip_global .eq. 0) then
       if (abs(sum_diff) .gt. 0.3) then
         print *, 'sum delta too large: ', sum_diff
@@ -116,8 +137,11 @@ program test_derivs
         print *, 'max delta too large: ', max_diff
       end if
     end if
+
   end if
+
 #ifdef MPI
   call MPI_Finalize(ierr)
 #endif
+
 end program test_derivs
