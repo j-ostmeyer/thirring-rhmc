790e5df705e80b40c7de984ce2034ad3a2e107c5
Makis Kappas <makis.kappas@gmail.com>
2020-02-04 10:43:20 +0000

    Added new Makefile and script to run tests

---
+++ tests/delRunall.sh
@@ -0,0 +1,13 @@
+source ./MkFlags
+
+MPI_RUNNER="mpirun -n $((${NP_X} * ${NP_Y} * ${NP_T} * ${NP_THIRD}))"
+
+TESTS=( test_dslash_1 test_dslash_3 test_dslash_5 \
+        test_dslashd_1 test_dslashd_3 test_dslashd_5 \
+				test_derivs test_qmrherm_0 )
+
+for test in "${TESTS[@]}"; do
+  echo "Testing " $test
+  $MPI_RUNNER $test
+  echo ""
+done
