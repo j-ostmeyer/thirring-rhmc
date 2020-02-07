6054ecb5ddc824e14f1bf5a32ed08e7922472eab
Makis Kappas <makis.kappas@gmail.com>
2020-02-07 14:49:44 +0000

    Revert back delMakefile & saved Michele's changes as delMakefile-Michele

--- tests/delRunall.sh
+++ tests/delRunall.sh
@@ -4,7 +4,7 @@ MPI_RUNNER="mpirun --oversubscribe -n $((${NP_X} * ${NP_Y} * ${NP_T} * ${NP_THIR
 
 TESTS=( test_dslash_1 test_dslash_3 test_dslash_5 \
         test_dslashd_1 test_dslashd_3 test_dslashd_5 \
-				test_derivs test_qmrherm_0 )
+				test_derivs test_qmrherm_0 test_qmrherm_1 test_qmrherm_2 test_qmrherm_3)
 
 for test in "${TESTS[@]}"; do
   echo "Testing " $test
