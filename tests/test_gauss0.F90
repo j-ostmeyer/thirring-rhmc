13519417006587d7372ece8f8f591f6239c22bad
Makis Kappas <makis.kappas@gmail.com>
2020-02-17 14:40:50 +0000

    gauss0 and gaussp subroutines are working with mpi along third dim

--- tests/test_gauss0.F90
+++ tests/test_gauss0.F90
@@ -1,13 +1,15 @@
 #include "test_utils.fh"
 program test_gauss0
-  use dwf3d_lib
+  ! use dwf3d_lib
+  use params
   use gauge
   use gaussian
   use comms
   use random
   implicit none
 
-  real :: ps(0:ksizex_l+1, 0:ksizey_l+1, 0:ksizet_l+1, 2)
+  real(dp) :: seed
+  real :: ps(0:ksizex_l + 1, 0:ksizey_l + 1, 0:ksizet_l + 1, 2)
   integer :: ix, iy, it, ix2, iy2, it2, i, j
   integer, dimension(4) :: duplicate_position1, duplicate_position2
   logical :: has_duplicates = .false.
@@ -26,8 +28,9 @@ program test_gauss0
   ! call function
 #ifdef MPI
   call gauss0(ps, reqs)
-  call complete_halo_update(reqs)
-#else 
+  ! call complete_halo_update(reqs)
+  call MPI_WaitAll(12, reqs, MPI_Statuses_Ignore, ierr)
+#else
   call gauss0(ps)
 #endif
 
@@ -35,6 +38,7 @@ program test_gauss0
   sumps = sum(ps(1:ksizex_l, 1:ksizey_l, 1:ksizet_l, :))
   maxps = maxval(ps(1:ksizex_l, 1:ksizey_l, 1:ksizet_l, :))
   minps = minval(ps(1:ksizex_l, 1:ksizey_l, 1:ksizet_l, :))
+
   outer: do it = 1, ksizet_l
     do it2 = 1, ksizet_l
       do iy = 1, ksizey_l
@@ -44,11 +48,11 @@ program test_gauss0
               do i = 1, 2
                 do j = 1, 2
                   if (ix .eq. ix2 .and. iy .eq. iy2 .and. it .eq. it2 &
-                    .and. i .eq. j) cycle
+                      .and. i .eq. j) cycle
                   if (ps(ix, iy, it, i) .eq. ps(ix2, iy2, it2, j)) then
                     has_duplicates = .true.
-                    duplicate_position1 = (/ ix, iy, it, i /)
-                    duplicate_position2 = (/ ix2, iy2, it2, j /)
+                    duplicate_position1 = (/ix, iy, it, i/)
+                    duplicate_position2 = (/ix2, iy2, it2, j/)
                     exit outer
                   endif
                 end do
@@ -59,36 +63,39 @@ program test_gauss0
       end do
     end do
   end do outer
-#ifdef MPI
-  call MPI_AllReduce(MPI_IN_PLACE, sumps, 1, MPI_REAL, MPI_SUM, comm,ierr)
-  call MPI_AllReduce(MPI_IN_PLACE, maxps, 1, MPI_REAL, MPI_MAX, comm,ierr)
-  call MPI_AllReduce(MPI_IN_PLACE, minps, 1, MPI_REAL, MPI_MIN, comm,ierr)
-  call MPI_AllReduce(MPI_IN_PLACE, has_duplicates, 1, MPI_LOGICAL, MPI_LOR, comm,ierr)
 
+#ifdef MPI
+  call MPI_AllReduce(MPI_IN_PLACE, sumps, 1, MPI_REAL, MPI_SUM, comm, ierr)
+  call MPI_AllReduce(MPI_IN_PLACE, maxps, 1, MPI_REAL, MPI_MAX, comm, ierr)
+  call MPI_AllReduce(MPI_IN_PLACE, minps, 1, MPI_REAL, MPI_MIN, comm, ierr)
+  call MPI_AllReduce(MPI_IN_PLACE, has_duplicates, 1, MPI_LOGICAL, MPI_LOR, comm, ierr)
 #endif
+
 #ifndef SITE_RANDOM
-#if defined(MPI) && NP_T > 1 && NP_X > 1 && NP_Y > 1
+#if defined(MPI) && (NP_T > 1 || NP_X > 1 || NP_Y > 1 || NP_THIRD > 1)
   if (ip_global .eq. 0) then
     print *, 'Unable to check results in parallel without using site_random'
   end if
 #else
-  check_float_equality(sumps, -15.65166, 0.001, 'sum', 'test_gauss0')
+  check_float_equality(sumps/np_third, -15.65166, 0.001, 'sum', 'test_gauss0')
   check_float_equality(maxps, 2.768291, 0.001, 'max', 'test_gauss0')
   check_float_equality(minps, -2.516524, 0.001, 'min', 'test_gauss0')
 #endif
 #else
-  check_float_equality(sumps, 14.460, 0.001, 'sum', 'test_gauss0')
+  check_float_equality(sumps/np_third, 14.460, 0.001, 'sum', 'test_gauss0')
   check_float_equality(maxps, 2.59536, 0.001, 'max', 'test_gauss0')
   check_float_equality(minps, -2.441575, 0.001, 'min', 'test_gauss0')
   if (ip_global .eq. 0) then
     if (has_duplicates) then
       print *, 'duplicate random numbers observed at:'
-      write(*, '(4i3)') duplicate_position1
-      write(*, '(4i3)') duplicate_position2
+      write (*, '(4i3)') duplicate_position1
+      write (*, '(4i3)') duplicate_position2
     end if
   end if
 #endif
+
 #ifdef MPI
   call MPI_Finalize(ierr)
 #endif
+
 end program
