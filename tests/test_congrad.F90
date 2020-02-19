478a8f9e6308d239a2a81feac2d079b9286a4dc9
Makis Kappas <makis.kappas@gmail.com>
2020-02-19 16:10:54 +0000

    congrad subroutine tested (pass)

--- tests/test_congrad.F90
+++ tests/test_congrad.F90
@@ -19,9 +19,9 @@ program test_congrad
   real*8, parameter :: tau = 8*atan(1.0_8)
 
   ! initialise function parameters
-  complex(dp) :: Phi(kthird, 0:ksizex_l + 1, 0:ksizey_l + 1, 0:ksizet_l + 1, 4)
-  complex(dp) :: x_ref(kthird, ksizex_l, ksizey_l, ksizet_l, 4)
-  complex(dp) :: diff(kthird, ksizex_l, ksizey_l, ksizet_l, 4)
+  complex(dp) :: Phi(0:kthird_l + 1, 0:ksizex_l + 1, 0:ksizey_l + 1, 0:ksizet_l + 1, 4)
+  complex(dp) :: x_ref(kthird_l, ksizex_l, ksizey_l, ksizet_l, 4)
+  complex(dp) :: diff(kthird_l, ksizex_l, ksizey_l, ksizet_l, 4)
   complex(dp) :: sum_diff
   real(dp) :: max_diff
 
@@ -33,7 +33,8 @@ program test_congrad
   integer, parameter :: idxmax = 4*ksize*ksize*ksizet*kthird
   integer :: idx = 0
 #ifdef MPI
-  integer, dimension(12) :: reqs_X, reqs_Phi, reqs_u
+  integer, dimension(16) :: reqs_X, reqs_Phi
+  integer, dimension(12) :: reqs_u
   integer :: ierr
 
   call init_MPI
@@ -51,11 +52,13 @@ program test_congrad
     do it = 1, ksizet_l
       do iy = 1, ksizey_l
         do ix = 1, ksizex_l
-          do ithird = 1, kthird
-            idx = ithird + (ip_x*ksizex_l + ix - 1)*kthird &
-              & + (ip_y*ksizey_l + iy - 1)*kthird*ksize &
-              & + (ip_t*ksizet_l + it - 1)*kthird*ksize*ksize &
-              & + (j - 1)*kthird*ksize*ksize*ksizet
+          do ithird = 1, kthird_l
+            idx = ip_third*kthird_l + ithird &
+                  + (ip_x*ksizex_l + ix - 1)*kthird &
+                  + (ip_y*ksizey_l + iy - 1)*kthird*ksize &
+                  + (ip_t*ksizet_l + it - 1)*kthird*ksize*ksize &
+                  + (j - 1)*kthird*ksize*ksize*ksizet
+
             Phi(ithird, ix, iy, it, j) = 1.1*exp(iunit*idx*tau/idxmax)
             X(ithird, ix, iy, it, j) = 0.5*exp(1.0)*exp(iunit*idx*tau/idxmax)
           enddo
@@ -63,51 +66,59 @@ program test_congrad
       enddo
     enddo
   enddo
+
 #ifdef MPI
   call start_halo_update_5(4, X, 0, reqs_X)
   call start_halo_update_5(4, Phi, 0, reqs_Phi)
 #endif
+
   idx = 0
   do j = 1, 3
     do it = 1, ksizet_l
       do iy = 1, ksizey_l
         do ix = 1, ksizex_l
           idx = ip_x*ksizex_l + ix &
-            & + (ip_y*ksizey_l + iy - 1)*ksize &
-            & + (ip_t*ksizet_l + it - 1)*ksize*ksize &
-            & + (j - 1)*ksize*ksize*ksizet
+                + (ip_y*ksizey_l + iy - 1)*ksize &
+                + (ip_t*ksizet_l + it - 1)*ksize*ksize &
+                + (j - 1)*ksize*ksize*ksizet
+
           u(ix, iy, it, j) = exp(iunit*idx*tau/idxmax)
         enddo
       enddo
     enddo
   enddo
+
 #ifdef MPI
   call start_halo_update_4(3, u, 1, reqs_u)
   call complete_halo_update(reqs_X)
   call complete_halo_update(reqs_Phi)
-  call complete_halo_update(reqs_u)
+  ! call complete_halo_update(reqs_u)
+  call MPI_WaitAll(12, reqs_u, MPI_Statuses_Ignore, ierr)
 #else
   call update_halo_5(4, Phi)
   call update_halo_5(4, X)
   call update_halo_4(3, u)
 #endif
+
   ! initialise common variables
   beta = 0.4
   am3 = 1.0
   ibound = -1
 
   call init_gammas()
+
   ! call function
   do i = 1, timing_loops
     call congrad(Phi, res, itercg, am, imass)
   end do
+
   ! check output
   if (generate) then
     write_file(x(:, 1:ksizex_l, 1:ksizey_l, 1:ksizet_l, :), 'test_congrad.dat', MPI_Double_Complex)
   else
     read_file(x_ref, 'test_congrad.dat', MPI_Double_Complex)
 
-    diff = x_ref - x(:, 1:ksizex_l, 1:ksizey_l, 1:ksizet_l, :)
+    diff = x_ref - x(1:kthird_l, 1:ksizex_l, 1:ksizey_l, 1:ksizet_l, :)
     sum_diff = sum(diff)
     max_diff = maxval(abs(diff))
 #ifdef MPI
@@ -126,5 +137,7 @@ program test_congrad
       end if
     end if
   end if
+
   call MPI_Finalize(ierr)
+
 end program
--- tests/test_congrad_1.F90
+++ tests/test_congrad_1.F90
@@ -18,7 +18,7 @@ program test_congrad_1
   real*8, parameter :: tau = 8*atan(1.0_8)
 
   ! initialise function parameters
-  complex(dp) :: Phi(kthird, 0:ksizex_l + 1, 0:ksizey_l + 1, 0:ksizet_l + 1, 4)
+  complex(dp) :: Phi(0:kthird_l + 1, 0:ksizex_l + 1, 0:ksizey_l + 1, 0:ksizet_l + 1, 4)
   complex(dp), allocatable :: delta_Phi(:, :, :, :, :)
   real(dp) :: sum_delta_Phi
 
@@ -31,11 +31,12 @@ program test_congrad_1
   integer, parameter :: idxmax = 4*ksize*ksize*ksizet*kthird
   integer :: idx = 0
 #ifdef MPI
-  integer, dimension(12) :: reqs_X, reqs_Phi, reqs_u
+  integer, dimension(16) :: reqs_X, reqs_Phi
+  integer, dimension(12) :: reqs_u
   integer :: ierr
   call init_MPI
 #endif
-  allocate (delta_Phi(kthird, ksizex_l, ksizey_l, ksizet_l, 4))
+  allocate (delta_Phi(kthird_l, ksizex_l, ksizey_l, ksizet_l, 4))
 
   h = 0
   hg = 0
@@ -52,11 +53,13 @@ program test_congrad_1
     do it = 1, ksizet_l
       do iy = 1, ksizey_l
         do ix = 1, ksizex_l
-          do ithird = 1, kthird
-            idx = ithird + (ip_x*ksizex_l + ix - 1)*kthird &
-              & + (ip_y*ksizey_l + iy - 1)*kthird*ksize &
-              & + (ip_t*ksizet_l + it - 1)*kthird*ksize*ksize &
-              & + (j - 1)*kthird*ksize*ksize*ksizet
+          do ithird = 1, kthird_l
+            idx = ip_third*kthird_l + ithird &
+                  + (ip_x*ksizex_l + ix - 1)*kthird &
+                  + (ip_y*ksizey_l + iy - 1)*kthird*ksize &
+                  + (ip_t*ksizet_l + it - 1)*kthird*ksize*ksize &
+                  + (j - 1)*kthird*ksize*ksize*ksizet
+
             Phi(ithird, ix, iy, it, j) = 1.1*exp(iunit*idx*tau/idxmax)
             X(ithird, ix, iy, it, j) = 0.5*exp(1.0)*exp(iunit*idx*tau/idxmax)
           enddo
@@ -64,34 +67,40 @@ program test_congrad_1
       enddo
     enddo
   enddo
+
 #ifdef MPI
   call start_halo_update_5(4, X, 0, reqs_X)
   call start_halo_update_5(4, Phi, 0, reqs_Phi)
 #endif
+
   idx = 0
   do j = 1, 3
     do it = 1, ksizet_l
       do iy = 1, ksizey_l
         do ix = 1, ksizex_l
           idx = ip_x*ksizex_l + ix &
-            & + (ip_y*ksizey_l + iy - 1)*ksize &
-            & + (ip_t*ksizet_l + it - 1)*ksize*ksize &
-            & + (j - 1)*ksize*ksize*ksizet
+                + (ip_y*ksizey_l + iy - 1)*ksize &
+                + (ip_t*ksizet_l + it - 1)*ksize*ksize &
+                + (j - 1)*ksize*ksize*ksizet
+
           u(ix, iy, it, j) = exp(iunit*idx*tau/idxmax)
         enddo
       enddo
     enddo
   enddo
+
 #ifdef MPI
   call start_halo_update_4(3, u, 1, reqs_u)
   call complete_halo_update(reqs_X)
   call complete_halo_update(reqs_Phi)
-  call complete_halo_update(reqs_u)
+  ! call complete_halo_update(reqs_u)
+  call MPI_WaitAll(12, reqs_u, MPI_Statuses_Ignore, ierr)
 #else
   call update_halo_5(4, Phi)
   call update_halo_5(4, X)
   call update_halo_4(3, u)
 #endif
+
   ! initialise common variables
   beta = 0.4
   am3 = 1.0
@@ -100,10 +109,11 @@ program test_congrad_1
   call init_gammas()
   ! call function
   call congrad(Phi, res, itercg, am, imass) ! results are in vector.x
+  
   ! check convergence
   call dirac_operator(xout, x, u, am, imass)
-  delta_Phi = Phi(:, 1:ksizex_l, 1:ksizey_l, 1:ksizet_l, :) - &
-    &                xout(:, 1:ksizex_l, 1:ksizey_l, 1:ksizet_l, :)
+  delta_Phi = Phi(1:kthird_l, 1:ksizex_l, 1:ksizey_l, 1:ksizet_l, :) &
+              - xout(1:kthird_l, 1:ksizex_l, 1:ksizey_l, 1:ksizet_l, :)
   delta_Phi = abs(delta_Phi)**2
 
   check_sum(delta_Phi, 1e-6, 'xout', sum_delta_Phi, MPI_Double_Precision, 'test_congrad_1')
@@ -113,4 +123,3 @@ program test_congrad_1
 #endif
 
 end program
-
