e32c261f341fc2d75e58ed34cc8e3882dc58de92
Makis Kappas <makis.kappas@gmail.com>
2020-03-04 11:03:46 +0000

    Modified halo tests to also check values along third dim. They pass.

--- tests/test_halo_5.F90
+++ tests/test_halo_5.F90
@@ -1,10 +1,10 @@
-pure function pid(fip_x, fip_y, fip_t) result(id)
+pure function pid(fip_third, fip_x, fip_y, fip_t) result(id)
   use params
   implicit none
-  integer, intent(in) :: fip_x, fip_y, fip_t
+  integer, intent(in) :: fip_third, fip_x, fip_y, fip_t
   integer :: id
 
-  id = fip_x + fip_y * np_x + fip_t * np_x * np_y
+  id = fip_third + (fip_x * np_third) + (fip_y * np_third * np_x) + (fip_t * np_third * np_x * np_y)
 end function pid
 
 
@@ -16,7 +16,7 @@ program test_halo_5
   integer :: ithird, ix, iy, it, i5, i=0
   integer :: pid
   integer :: passed_basic = 0
-  complex(dp) :: test_array(kthird, 0:ksizex_l+1, 0:ksizey_l+1, 0:ksizet_l+1, 4)
+  complex(dp) :: test_array(0:kthird_l+1, 0:ksizex_l+1, 0:ksizey_l+1, 0:ksizet_l+1, 4)
 #ifdef MPI
   integer :: reqs(16)
   integer :: ierr
@@ -24,9 +24,11 @@ program test_halo_5
   call init_MPI
 #endif
 
+  test_array(0,:,:,:,:) = cmplx(-4,-1)
   test_array(:,0,:,:,:) = cmplx(-1,-1)
   test_array(:,:,0,:,:) = cmplx(-2,-1)
   test_array(:,:,:,0,:) = cmplx(-3,-1)
+  test_array(0:kthird_l+1,:,:,:,:) = cmplx(-4,-2)
   test_array(:,ksizex_l+1,:,:,:) = cmplx(-1,-2)
   test_array(:,:,ksizey_l+1,:,:) = cmplx(-2,-2)
   test_array(:,:,:,ksizet_l+1,:) = cmplx(-3,-2)
@@ -36,9 +38,9 @@ program test_halo_5
     do it=1,ksizet_l
       do iy=1,ksizey_l
         do ix=1,ksizex_l
-          do ithird=1,kthird
+          do ithird=1,kthird_l
             i = i + 1
-            test_array(ithird, ix, iy, it, i5) = cmplx(i, pid(ip_x, ip_y, ip_t))
+            test_array(ithird, ix, iy, it, i5) = cmplx(i, pid(ip_third, ip_x, ip_y, ip_t))
           end do
         end do
       end do
@@ -54,43 +56,47 @@ program test_halo_5
 #endif
 
   ! Check output
+  if (real(test_array(1,1,1,1,1)) .ne. real(test_array(kthird_l+1,1,1,1,1)) .or. &
+      nint(aimag(test_array(kthird_l+1,1,1,1,1))) .ne. pid(modulo(ip_third+1, np_third), ip_x, ip_y, ip_t)) then
+    print *, "Negative third update failed on process", ip_x, ip_y, ip_t, ip_third
+  end if
   if (real(test_array(1,1,1,1,1)) .ne. real(test_array(1,ksizex_l+1,1,1,1)) .or. &
-    nint(aimag(test_array(1,ksizex_l+1,1,1,1))) &
-    .ne. pid(modulo(ip_x+1, np_x), ip_y, ip_t)) then
-    print *, "Negative x update failed on process", ip_x, ip_y, ip_t
+      nint(aimag(test_array(1,ksizex_l+1,1,1,1))) .ne. pid(ip_third, modulo(ip_x+1, np_x), ip_y, ip_t)) then
+    print *, "Negative x update failed on process", ip_x, ip_y, ip_t, ip_third
   end if
   if (real(test_array(1,1,1,1,1)) .ne. real(test_array(1,1,ksizey_l+1,1,1)) .or. &
-    nint(aimag(test_array(1,1,ksizey_l+1,1,1))) &
-    .ne. pid(ip_x, modulo(ip_y+1, np_y), ip_t)) then
-    print *, "Negative y update failed on process", ip_x, ip_y, ip_t
+      nint(aimag(test_array(1,1,ksizey_l+1,1,1))) .ne. pid(ip_third, ip_x, modulo(ip_y+1, np_y), ip_t)) then
+    print *, "Negative y update failed on process", ip_x, ip_y, ip_t, ip_third
   end if
   if (real(test_array(1,1,1,1,1)) .ne. real(test_array(1,1,1,ksizet_l+1,1)) .or. &
-    nint(aimag(test_array(1,1,1,ksizet_l+1,1))) &
-    .ne. pid(ip_x, ip_y, modulo(ip_t+1, np_t))) then
-    print *, "Negative t update failed on process", ip_x, ip_y, ip_t
+      nint(aimag(test_array(1,1,1,ksizet_l+1,1))) .ne. pid(ip_third, ip_x, ip_y, modulo(ip_t+1, np_t))) then
+    print *, "Negative t update failed on process", ip_x, ip_y, ip_t, ip_third
+  end if
+
+  if (real(test_array(kthird_l,1,1,1,1)) .ne. real(test_array(0,1,1,1,1)) .or. &
+      nint(aimag(test_array(0,1,1,1,1))) .ne. pid(modulo(ip_third-1, np_third), ip_x, ip_y, ip_t)) then
+    print *, "Positive third update failed on process", ip_x, ip_y, ip_t, ip_third
   end if
   if (real(test_array(1,ksizex_l,1,1,1)) .ne. real(test_array(1,0,1,1,1)) .or. &
-    nint(aimag(test_array(1,0,1,1,1))) &
-    .ne. pid(modulo(ip_x-1, np_x), ip_y, ip_t)) then
-    print *, "Positive x update failed on process", ip_x, ip_y, ip_t
+      nint(aimag(test_array(1,0,1,1,1))) .ne. pid(ip_third, modulo(ip_x-1, np_x), ip_y, ip_t)) then
+    print *, "Positive x update failed on process", ip_x, ip_y, ip_t, ip_third
   end if
   if (real(test_array(1,1,ksizey_l,1,1)) .ne. real(test_array(1,1,0,1,1)) .or. &
-    nint(aimag(test_array(1,1,0,1,1))) &
-    .ne. pid(ip_x, modulo(ip_y-1, np_y), ip_t)) then
-    print *, "Positive y update failed on process", ip_x, ip_y, ip_t
+      nint(aimag(test_array(1,1,0,1,1))) .ne. pid(ip_third, ip_x, modulo(ip_y-1, np_y), ip_t)) then
+    print *, "Positive y update failed on process", ip_x, ip_y, ip_t, ip_third
   end if
   if (real(test_array(1,1,1,ksizet_l,1)) .ne. real(test_array(1,1,1,0,1)) .or. &
-    nint(aimag(test_array(1,1,1,0,1))) &
-    .ne. pid(ip_x, ip_y, modulo(ip_t-1, np_t))) then
-    print *, "Positive t update failed on process", ip_x, ip_y, ip_t
+      nint(aimag(test_array(1,1,1,0,1))) .ne. pid(ip_third, ip_x, ip_y, modulo(ip_t-1, np_t))) then
+    print *, "Positive t update failed on process", ip_x, ip_y, ip_t, ip_third
   else
     passed_basic = 1
   end if
-  if (passed_basic.eq.1 .and. &
-    (real(test_array(1,1,1,ksizet_l,4)) .ne. real(test_array(1,1,1,0,4)) .or. &
-    nint(aimag(test_array(1,1,1,0,4))) &
-    .ne. pid(ip_x, ip_y, modulo(ip_t-1, np_t)))) then
-    print *, "Extent is wrong in size5 direction; process", ip_x, ip_y, ip_t
+  
+  if (passed_basic .eq.1 .and. &
+      (real(test_array(1,1,1,ksizet_l,4)) .ne. real(test_array(1,1,1,0,4)) .or. &
+       nint(aimag(test_array(1,1,1,0,4))) &
+       .ne. pid(ip_third, ip_x, ip_y, modulo(ip_t-1, np_t)))) then
+    print *, "Extent is wrong in size5 direction; process", ip_x, ip_y, ip_t, ip_third
   end if
 
 
@@ -98,4 +104,3 @@ program test_halo_5
   call MPI_Finalize(ierr)
 #endif
 end program test_halo_5
-
--- tests/test_halo_6.F90
+++ tests/test_halo_6.F90
@@ -1,10 +1,10 @@
-pure function pid(ix, iy, it) result(id)
+pure function pid(ithird, ix, iy, it) result(id)
   use params
   implicit none
-  integer, intent(in) :: ix, iy, it
+  integer, intent(in) :: ithird, ix, iy, it
   integer :: id
 
-  id = ix + iy * np_x + it * np_x * np_y
+  id = ithird + (ix * np_third) + (iy * np_third * np_x) + (it * np_third * np_x * np_y)
 end function pid
 
 program test_halo_6
@@ -15,16 +15,18 @@ program test_halo_6
   integer :: ithird, ix, iy, it, i5, i6, i=0
   integer :: pid
   integer :: passed_basic = 0
-  complex(dp) :: test_array(kthird, 0:ksizex_l+1, 0:ksizey_l+1, 0:ksizet_l+1, 4, 12)
+  complex(dp) :: test_array(0:kthird_l+1, 0:ksizex_l+1, 0:ksizey_l+1, 0:ksizet_l+1, 4, 12)
 #ifdef MPI
   integer :: reqs(16)
   integer :: ierr
   call init_MPI
 #endif
 
+  test_array(0,:,:,:,:,:) = cmplx(-4,-1)
   test_array(:,0,:,:,:,:) = cmplx(-1,-1)
   test_array(:,:,0,:,:,:) = cmplx(-2,-1)
   test_array(:,:,:,0,:,:) = cmplx(-3,-1)
+  test_array(kthird_l+1,:,:,:,:,:) = cmplx(-4,-2)
   test_array(:,ksizex_l+1,:,:,:,:) = cmplx(-1,-2)
   test_array(:,:,ksizey_l+1,:,:,:) = cmplx(-2,-2)
   test_array(:,:,:,ksizet_l+1,:,:) = cmplx(-3,-2)
@@ -35,9 +37,9 @@ program test_halo_6
       do it=1,ksizet_l
         do iy=1,ksizey_l
           do ix=1,ksizex_l
-            do ithird=1,kthird
+            do ithird=1,kthird_l
               i = i + 1
-              test_array(ithird, ix, iy, it, i5, i6) = cmplx(i, pid(ip_x, ip_y, ip_t))
+              test_array(ithird, ix, iy, it, i5, i6) = cmplx(i, pid(ip_third, ip_x, ip_y, ip_t))
             end do
           end do
         end do
@@ -52,50 +54,63 @@ program test_halo_6
 #else
   call update_halo_6(4, 12, test_array)
 #endif
+
   ! Check output
+  if (real(test_array(1,1,1,1,1,1)) .ne. real(test_array(kthird_l+1,1,1,1,1,1)) .or. &
+    nint(aimag(test_array(kthird_l+1,1,1,1,1,1))) &
+    .ne. pid(modulo(ip_third+1, np_third), ip_x, ip_y, ip_t)) then
+    print *, "Negative third update failed on process", ip_x, ip_y, ip_t, ip_third
+  end if
   if (real(test_array(1,1,1,1,1,1)) .ne. real(test_array(1,ksizex_l+1,1,1,1,1)) .or. &
     nint(aimag(test_array(1,ksizex_l+1,1,1,1,1))) &
-    .ne. pid(modulo(ip_x+1, np_x), ip_y, ip_t)) then
-    print *, "Negative x update failed on process", ip_x, ip_y, ip_t
+    .ne. pid(ip_third, modulo(ip_x+1, np_x), ip_y, ip_t)) then
+    print *, "Negative x update failed on process", ip_x, ip_y, ip_t, ip_third
   end if
   if (real(test_array(1,1,1,1,1,1)) .ne. real(test_array(1,1,ksizey_l+1,1,1,1)) .or. &
     nint(aimag(test_array(1,1,ksizey_l+1,1,1,1))) &
-    .ne. pid(ip_x, modulo(ip_y+1, np_y), ip_t)) then
-    print *, "Negative y update failed on process", ip_x, ip_y, ip_t
+    .ne. pid(ip_third, ip_x, modulo(ip_y+1, np_y), ip_t)) then
+    print *, "Negative y update failed on process", ip_x, ip_y, ip_t, ip_third
   end if
   if (real(test_array(1,1,1,1,1,1)) .ne. real(test_array(1,1,1,ksizet_l+1,1,1)) .or. &
     nint(aimag(test_array(1,1,1,ksizet_l+1,1,1))) &
-    .ne. pid(ip_x, ip_y, modulo(ip_t+1, np_t))) then
-    print *, "Negative t update failed on process", ip_x, ip_y, ip_t
+    .ne. pid(ip_third, ip_x, ip_y, modulo(ip_t+1, np_t))) then
+    print *, "Negative t update failed on process", ip_x, ip_y, ip_t, ip_third
+  end if
+
+  if (real(test_array(kthird_l,1,1,1,1,1)) .ne. real(test_array(0,1,1,1,1,1)) .or. &
+    nint(aimag(test_array(0,1,1,1,1,1))) &
+    .ne. pid(modulo(ip_third-1, np_third), ip_x, ip_y, ip_t)) then
+    print *, "Positive third update failed on process", ip_x, ip_y, ip_t, ip_third
   end if
   if (real(test_array(1,ksizex_l,1,1,1,1)) .ne. real(test_array(1,0,1,1,1,1)) .or. &
     nint(aimag(test_array(1,0,1,1,1,1))) &
-    .ne. pid(modulo(ip_x-1, np_x), ip_y, ip_t)) then
-    print *, "Positive x update failed on process", ip_x, ip_y, ip_t
+    .ne. pid(ip_third, modulo(ip_x-1, np_x), ip_y, ip_t)) then
+    print *, "Positive x update failed on process", ip_x, ip_y, ip_t, ip_third
   end if
   if (real(test_array(1,1,ksizey_l,1,1,1)) .ne. real(test_array(1,1,0,1,1,1)) .or. &
     nint(aimag(test_array(1,1,0,1,1,1))) &
-    .ne. pid(ip_x, modulo(ip_y-1, np_y), ip_t)) then
-    print *, "Positive y update failed on process", ip_x, ip_y, ip_t
+    .ne. pid(ip_third, ip_x, modulo(ip_y-1, np_y), ip_t)) then
+    print *, "Positive y update failed on process", ip_x, ip_y, ip_t, ip_third
   end if
   if (real(test_array(1,1,1,ksizet_l,1,1)) .ne. real(test_array(1,1,1,0,1,1)) .or. &
     nint(aimag(test_array(1,1,1,0,1,1))) &
-    .ne. pid(ip_x, ip_y, modulo(ip_t-1, np_t))) then
-    print *, "Positive t update failed on process", ip_x, ip_y, ip_t
+    .ne. pid(ip_third, ip_x, ip_y, modulo(ip_t-1, np_t))) then
+    print *, "Positive t update failed on process", ip_x, ip_y, ip_t, ip_third
   else
     passed_basic = 1
   end if
+
   if (passed_basic.eq.1 .and. &
     (real(test_array(1,1,1,ksizet_l,4,1)) .ne. real(test_array(1,1,1,0,4,1)) .or. &
     nint(aimag(test_array(1,1,1,0,4,1))) &
-    .ne. pid(ip_x, ip_y, modulo(ip_t-1, np_t)))) then
-    print *, "Extent is wrong in size5 direction; process", ip_x, ip_y, ip_t
+    .ne. pid(ip_third, ip_x, ip_y, modulo(ip_t-1, np_t)))) then
+    print *, "Extent is wrong in size5 direction; process", ip_x, ip_y, ip_t, ip_third
   end if
   if (passed_basic.eq.1 .and. &
     (real(test_array(1,1,1,ksizet_l,1,12)) .ne. real(test_array(1,1,1,0,1,12)) .or. &
     nint(aimag(test_array(1,1,1,0,1,12))) &
-    .ne. pid(ip_x, ip_y, modulo(ip_t-1, np_t)))) then
-    print *, "Extent is wrong in size6 direction; process", ip_x, ip_y, ip_t
+    .ne. pid(ip_third, ip_x, ip_y, modulo(ip_t-1, np_t)))) then
+    print *, "Extent is wrong in size6 direction; process", ip_x, ip_y, ip_t, ip_third
   end if
 
 
@@ -103,4 +118,3 @@ program test_halo_6
   call MPI_Finalize(ierr)
 #endif
 end program test_halo_6
-
