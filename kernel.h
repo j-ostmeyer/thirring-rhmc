e301f63dcdb7f82de0d69d5e8ab52aaddc73f686
Johann Ostmeyer <ostmeyer@hiskp.uni-bonn.de>
2023-10-24 12:29:00 +0100

    kernel header file

---
+++ kernel.h
@@ -0,0 +1,19 @@
+#ifndef NEWKERNEL
+#define NEWKERNEL
+#endif
+
+#ifndef WILSONKERNEL
+#define WILSONKERNEL
+#endif
+
+#ifndef SHAMIRKERNEL
+#define SHAMIRKERNELL
+#endif
+
+#ifndef DERIVSWILSON
+#define DERIVSWILSON
+#endif
+ 
+#ifndef MEASUREWILSON
+#define MEASUREWILSON
+#endif
