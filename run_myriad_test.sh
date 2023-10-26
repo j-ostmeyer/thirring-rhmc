76de215a4485f92fc6ab291603d0af40ca8ddf98
caird <c.aird@ucl.ac.uk>
2023-10-26 17:48:19 +0100

    Fixing script

--- run_myriad_test.sh
+++ run_myriad_test.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 
-SAMPLES_DIR="./samples"
+SAMPLES_DIR="samples"
 CONS_DIR="${SAMPLES_DIR}/cons"
-MIDOUT_file="${SAMPLES_DIR}/midout"
+MIDOUT_FILE="${SAMPLES_DIR}/midout"
 MYRIAD_SUBMIT_FILE="myriad_submit.sh"
 
 # get inputs from cli
-if [[ ( -z "${KSIZE}" )  || ( -z "${KSIZET}" )   || \
-	  ( -z "${KTHIRD}" ) || ( -z "${ITER2}" )    || \
-	  ( -z "${NP_X}" )   || ( -z "${NP_Y}" )     || \
-	  ( -z "${NP_T}" )   || ( -z "${NP_THIRD}" ) || ]]; then
-	echo "Please ensure, the env vars KSIZE, KSIZET, KTHIRD and ITER2 are define"
+if [[ ( -z "${KSIZE}" ) || ( -z "${KSIZET}" ) || \
+	  ( -z "${KTHIRD}" ) || ( -z "${ITER2}" ) || \
+	  ( -z "${NP_X}" ) || ( -z "${NP_Y}" ) || \
+	  ( -z "${NP_T}" ) || ( -z "${NP_THIRD}" ) ]]; then
+	echo "Please ensure, the env vars KSIZE, KSIZET, KTHIRD, ITER2, NP_X, NP_Y, NP_T and NP_THIRD are defined"
 	exit 0
 else
 	echo "Using config:"
-	echo "  KSIZE:  ${KSIZE}"
-	echo "  KSIZET: ${KSIZET}"
-	echo "  KTHIRD: ${KTHIRD}"
-	echo "  NP_X:  ${NP_X}"
-	echo "  NP_Y:  ${NP_Y}"
-	echo "  NP_T:  ${NP_T}"
-	echo "  NP_THIRD:  ${NP_THIRD}"
+	echo "  KSIZE:    ${KSIZE}"
+	echo "  KSIZET:   ${KSIZET}"
+	echo "  KTHIRD:   ${KTHIRD}"
+	echo "  ITER2:    ${ITER2}"
+	echo "  NP_X:     ${NP_X}"
+	echo "  NP_Y:     ${NP_Y}"
+	echo "  NP_T:     ${NP_T}"
+	echo "  NP_THIRD: ${NP_THIRD}"
 fi
 
 # Check we have a con file matching the inputs
@@ -33,35 +34,38 @@ else
 fi
 
 # Create output folder TEST_OUTPUT_<KSIZE>_<KSIZET>_<KTHIRD>_<ITER2>
-OUTPUT_DIR="TEST_OUTPUT_${KSIZE}_${KSIZET}_${KTHIRD}_${IETR2}"
+OUTPUT_DIR="TEST_OUTPUT_${KSIZE}_${KSIZET}_${KTHIRD}_${ITER2}_${NP_X}_${NP_Y}_${NP_T}_${NP_THIRD}"
 mkdir $OUTPUT_DIR
 
-# replace iter2 in midout with user input
-sed "s/<ITER2>/${ITER2}/g" "${OUTPUT_DIR}/midout"
+# Copy myriad_submit.sh, con, midout and remez* files from samples into output folder
+cp $MYRIAD_SUBMIT_FILE "./${OUTPUT_DIR}/${MYRIAD_SUBMIT_FILE}"
+cp $CON_FILE "./${OUTPUT_DIR}/con"
+cp $MIDOUT_FILE "./${OUTPUT_DIR}/midout"
+cp remez2 remez2g remez4 remez4g "${OUTPUT_DIR}/"
 
 # update params.F90 with user inputted KSIZE* values
-sed "s/#define KSIZE .*/#define KSIZE ${KSIZE}/g" params.F90
-sed "s/#define KSIZET .*/#define KSIZET ${KSIZET}/g" params.F90
-sed "s/#define KTHIRD .*/#define KTHIRD ${KTHIRD}/g" params.F90
+sed -i '' "s/#define KSIZE .*/#define KSIZE ${KSIZE}/g" params.F90
+sed -i '' "s/#define KSIZET .*/#define KSIZET ${KSIZET}/g" params.F90
+sed -i '' "s/#define KTHIRD .*/#define KTHIRD ${KTHIRD}/g" params.F90
 
 # Update MkFlags with NP values 
-sed "s/NP_X=.*/NP_X=${NP_X}/g" MkFlags
-sed "s/NP_Y=.*/NP_Y=${NP_Y}/g" MkFlags
-sed "s/NP_T=.*/NP_T=${NP_T}/g" MkFlags
-sed "s/NP_THIRD=.*/NP_THIRD=${NP_THIRD}/g" MkFlags
+sed -i '' "s/NP_X=.*/NP_X=${NP_X}/g" MkFlags
+sed -i '' "s/NP_Y=.*/NP_Y=${NP_Y}/g" MkFlags
+sed -i '' "s/NP_T=.*/NP_T=${NP_T}/g" MkFlags
+sed -i '' "s/NP_THIRD=.*/NP_THIRD=${NP_THIRD}/g" MkFlags
 
 # run make
-make
+echo "make"
+#make
+
+# replace iter2 in midout with user input
+sed -i '' "s/<ITER2>/${ITER2}/g" "${OUTPUT_DIR}/midout"
 
 # Update submission script with total NP
-NP_TOTAL="$(($NP_X + $NP_Y + $NP_T + $NP_THIRD))"
+NP_TOTAL="$(($NP_X * $NP_Y * $NP_T * $NP_THIRD))"
+echo "NP_TOTAL: ${NP_TOTAL}"
 echo "Total number of processors: ${NP_TOTAL}"
-sed "s/#$ -pe mpi .*/#$ -pe mpi ${NP_TOTAL}/g" myriad_submit.sh
-
-# Copy myriad_submit.sh, con and midout files from samples into output folder
-cp $MYRIAD_SUBMIT_FILE "${OUTPUT_DIR}/${MYRIAD_SUBMIT_FILE}"
-cp $CON_FILE "${OUTPUT_DIR}/con"
-cp $MIDOUT_FILE "${OUTPUT_DIR}/midout"
+sed -i '' "s/#$ -pe mpi .*/#$ -pe mpi ${NP_TOTAL}/g" "${OUTPUT_DIR}/myriad_submit.sh"
 
 # submit job
 cd $OUTPUT_DIR
