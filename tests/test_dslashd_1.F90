27ae1dab543336b654444f9e85d9b6e4ef41213d
caird <c.aird@ucl.ac.uk>
2023-11-01 11:49:04 +0000

    Fix test_dslash_1.F90

--- tests/test_dslashd_1.F90
+++ tests/test_dslashd_1.F90
@@ -19,10 +19,11 @@ program test_dslashd
 
   ! initialise function parameters
   complex(dp) u(0:ksizex_l + 1, 0:ksizey_l + 1, 0:ksizet_l + 1, 3)
-  complex(dp) Phi(kthird, 0:ksizex_l + 1, 0:ksizey_l + 1, 0:ksizet_l + 1, 4)
-  complex(dp) Phiref(kthird, ksizex_l, ksizey_l, ksizet_l, 4)
-  complex(dp) R(kthird, 0:ksizex_l + 1, 0:ksizey_l + 1, 0:ksizet_l + 1, 4)
-  complex(dp) diff(kthird, ksizex_l, ksizey_l, ksizet_l, 4)
+  complex(dp) Phi(0:kthird_l + 1, 0:ksizex_l + 1, 0:ksizey_l + 1, 0:ksizet_l + 1, 4)
+  complex(dp) PhiToWrite(kthird_l, ksizex_l, ksizey_l, ksizet_l, 4)
+  complex(dp) Phiref(kthird_l, ksizex_l, ksizey_l, ksizet_l, 4)
+  complex(dp) R(0:kthird_l + 1, 0:ksizex_l + 1, 0:ksizey_l + 1, 0:ksizet_l + 1, 4)
+  complex(dp) diff(kthird_l, ksizex_l, ksizey_l, ksizet_l, 4)
   complex(dp) sum_diff
   real(dp) max_diff
 
@@ -99,7 +100,9 @@ program test_dslashd
   end do
   ! check output
   if (generate) then
-    write_file(Phi(:, 1:ksizex_l, 1:ksizey_l, 1:ksizet_l, :), 'test_dslashd_1.dat', MPI_Double_Complex)
+    print *, "Generating .dat file..."
+    PhiToWrite(:, :, :, :, :) = Phi(1:kthird_l, 1:ksizex_l, 1:ksizey_l, 1:ksizet_l, :)
+    write_file(PhiToWrite, 'test_dslash_1.dat', MPI_Double_Complex)
   else
     read_file(Phiref, 'test_dslashd_1.dat', MPI_Double_Complex)
 
