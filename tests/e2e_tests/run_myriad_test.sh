0680cdf42be4f7856bb39ca222f8b373f86bc908
caird <c.aird@ucl.ac.uk>
2023-11-02 14:30:46 +0000

    Add flag to turn on/off meson/measure

--- tests/e2e_tests/run_myriad_test.sh
+++ tests/e2e_tests/run_myriad_test.sh
@@ -6,6 +6,9 @@ CONS_DIR="${SAMPLES_DIR}/cons"
 MIDOUT_FILE="${SAMPLES_DIR}/midout"
 MYRIAD_SUBMIT_FILE="myriad_submit.sh"
 
+# optional inputs
+USE_MEASURE="${USE_MEASURE:-"yes"}"
+
 # get inputs from cli
 if [[ ( -z "${KSIZE}" ) || ( -z "${KSIZET}" ) || \
 	  ( -z "${KTHIRD}" ) || ( -z "${ITER2}" ) || \
@@ -15,14 +18,15 @@ if [[ ( -z "${KSIZE}" ) || ( -z "${KSIZET}" ) || \
 	exit 0
 else
 	echo "Using config:"
-	echo "  KSIZE:    ${KSIZE}"
-	echo "  KSIZET:   ${KSIZET}"
-	echo "  KTHIRD:   ${KTHIRD}"
-	echo "  ITER2:    ${ITER2}"
-	echo "  NP_X:     ${NP_X}"
-	echo "  NP_Y:     ${NP_Y}"
-	echo "  NP_T:     ${NP_T}"
-	echo "  NP_THIRD: ${NP_THIRD}"
+	echo "  KSIZE:       ${KSIZE}"
+	echo "  KSIZET:      ${KSIZET}"
+	echo "  KTHIRD:      ${KTHIRD}"
+	echo "  ITER2:       ${ITER2}"
+	echo "  NP_X:        ${NP_X}"
+	echo "  NP_Y:        ${NP_Y}"
+	echo "  NP_T:        ${NP_T}"
+	echo "  NP_THIRD:    ${NP_THIRD}"
+	echo "  USE_MEASURE: ${USE_MEASURE}"
 fi
 
 # Check we have a con file matching the inputs
@@ -35,7 +39,7 @@ else
 fi
 
 # Create output folder TEST_OUTPUT_<KSIZE>_<KSIZET>_<KTHIRD>_<ITER2>
-OUTPUT_DIR="TEST_OUTPUT_${KSIZE}_${KSIZET}_${KTHIRD}_${ITER2}_${NP_X}_${NP_Y}_${NP_T}_${NP_THIRD}"
+OUTPUT_DIR="TEST_OUTPUT_${KSIZE}_${KSIZET}_${KTHIRD}_${ITER2}_${NP_X}_${NP_Y}_${NP_T}_${NP_THIRD}_${USE_MEASURE}"
 if [ -d "${OUTPUT_DIR}" ]; then
 	echo "Error: A submission for these inputs has already been made"
 	exit 0
@@ -44,14 +48,15 @@ mkdir $OUTPUT_DIR
 
 touch "${OUTPUT_DIR}/inputs"
 echo "Using config:" >> "${OUTPUT_DIR}/inputs"
-echo "  KSIZE:    ${KSIZE}" >> "${OUTPUT_DIR}/inputs"
-echo "  KSIZET:   ${KSIZET}" >> "${OUTPUT_DIR}/inputs"
-echo "  KTHIRD:   ${KTHIRD}" >> "${OUTPUT_DIR}/inputs"
-echo "  ITER2:    ${ITER2}" >> "${OUTPUT_DIR}/inputs"
-echo "  NP_X:     ${NP_X}" >> "${OUTPUT_DIR}/inputs"
-echo "  NP_Y:     ${NP_Y}" >> "${OUTPUT_DIR}/inputs"
-echo "  NP_T:     ${NP_T}" >> "${OUTPUT_DIR}/inputs"
-echo "  NP_THIRD: ${NP_THIRD}" >> "${OUTPUT_DIR}/inputs"
+echo "  KSIZE:       ${KSIZE}" >> "${OUTPUT_DIR}/inputs"
+echo "  KSIZET:      ${KSIZET}" >> "${OUTPUT_DIR}/inputs"
+echo "  KTHIRD:      ${KTHIRD}" >> "${OUTPUT_DIR}/inputs"
+echo "  ITER2:       ${ITER2}" >> "${OUTPUT_DIR}/inputs"
+echo "  NP_X:        ${NP_X}" >> "${OUTPUT_DIR}/inputs"
+echo "  NP_Y:        ${NP_Y}" >> "${OUTPUT_DIR}/inputs"
+echo "  NP_T:        ${NP_T}" >> "${OUTPUT_DIR}/inputs"
+echo "  NP_THIRD:    ${NP_THIRD}" >> "${OUTPUT_DIR}/inputs"
+echo "  USE_MEASURE: ${USE_MEASURE}" >> "${OUTPUT_DIR}/inputs"
 
 # Copy myriad_submit.sh, con, midout and remez* files from samples into output folder
 cp $MYRIAD_SUBMIT_FILE "./${OUTPUT_DIR}/${MYRIAD_SUBMIT_FILE}"
@@ -65,6 +70,15 @@ sed -i "s/#define KSIZET .*/#define KSIZET ${KSIZET}/g" "${SOURCE_DIR}/params.F9
 sed -i "s/#define KTHIRD .*/#define KTHIRD ${KTHIRD}/g" "${SOURCE_DIR}/params.F90"
 sed -i "s/integer, parameter :: istart = .*/integer, parameter :: istart = -1/g" "${SOURCE_DIR}/params.F90"
 sed -i "s/integer, parameter :: iread = .*/integer, parameter :: iread = 1/g" "${SOURCE_DIR}/params.F90"
+
+# Switch between meson and measure
+if [ $USE_MEASURE -eq "yes" ]; then
+	sed -i "s/!call measure(pbp, respbp, ancgm, am, imass, isweep + isweep_total_start)/ call measure(pbp, respbp, ancgm, am, imass, isweep + isweep_total_start)/g" "${SOURCE_DIR}/dwf3d_lib.F90"
+	sed -i "s/ call meson(rescgm,itercg,ancgm,am,imass, isweep + isweep_total_start)/!call meson(rescgm,itercg,ancgm,am,imass, isweep + isweep_total_start)/g" "${SOURCE_DIR}/dwf3d_lib.F90"
+else
+	sed -i "s/ call measure(pbp, respbp, ancgm, am, imass, isweep + isweep_total_start)/!call measure(pbp, respbp, ancgm, am, imass, isweep + isweep_total_start)/g" "${SOURCE_DIR}/dwf3d_lib.F90"
+	sed -i "s/!call meson(rescgm,itercg,ancgm,am,imass, isweep + isweep_total_start)/ call meson(rescgm,itercg,ancgm,am,imass, isweep + isweep_total_start)/g" "${SOURCE_DIR}/dwf3d_lib.F90"
+fi
 
 # Update MkFlags with NP values 
 sed -i "s/NP_X=.*/NP_X=${NP_X}/g" "${SOURCE_DIR}/MkFlags"
