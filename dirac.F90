13188366c7c48399eb0911a4e791e64a74223b47
Jamie Quinn <jamiejquinn@jamiejquinn.com>
2023-11-02 16:35:45 +0000

    Add Wilson kernel versions of dslash and dslashd. Tests of those passing.

--- dirac.F90
+++ dirac.F90
@@ -16,7 +16,8 @@ contains
     integer, intent(in) :: imass
     real, intent(in) :: am
 
-    call dslash_shamir(Phi, R, u, am, imass)
+    ! call dslash_shamir(Phi, R, u, am, imass)
+    call dslash_wilson(Phi, R, u, am, imass)
   end subroutine
 
   pure subroutine dslash_shamir(Phi, R, u, am, imass)
@@ -171,6 +172,126 @@ contains
     return
   end subroutine dslash_shamir
 
+  pure subroutine DWilson(Phi,R,u,mass)
+    implicit none
+    complex(dp), intent(out) :: Phi(0:ksizex_l+1, 0:ksizey_l+1, 0:ksizet_l+1, 4)
+    complex(dp), intent(in) :: R(0:ksizex_l+1, 0:ksizey_l+1, 0:ksizet_l+1, 4)
+    complex(dp), intent(in) :: u(0:ksizex_l+1, 0:ksizey_l+1, 0:ksizet_l+1, 3)
+    real, intent(in) :: mass
+    real(dp) diag
+    integer mu,idirac,it,iy,ix,ixup,iyup,itup,igork
+
+    ! diagonal term
+    diag=(3.0+mass)! 3 is from Wilson term
+    Phi=diag*R
+    !      
+    !     Wilson term (hermitian) and Dirac term (antihermitian)
+    do mu=1,3
+      ixup = kdelta(1, mu)
+      iyup = kdelta(2, mu)
+      itup = kdelta(3, mu)
+
+      do idirac=1,4
+        igork=gamin(mu,idirac)
+        do it = 1,ksizet_l
+          do iy = 1,ksizey_l
+            do ix = 1,ksizex_l
+              Phi(ix,iy,it,idirac)=Phi(ix,iy,it,idirac) &
+                ! Wilson term (hermitian)
+              &    -akappa*(u(ix,iy,it,mu) &
+                &              * R(ix+ixup, iy+iyup, it+itup, idirac) &
+                &             + conjg(u(ix-ixup, iy-iyup, it-itup, mu)) &
+                &              * R(ix-ixup, iy-iyup, it-itup, idirac)) &
+               ! Dirac term (antihermitian)
+              &     + gamval(mu,idirac) * &
+                &       (u(ix,iy,it,mu) &
+                &         * R(ix+ixup, iy+iyup, it+itup, igork) &
+                &        - conjg(u(ix-ixup, iy-iyup, it-itup, mu)) &
+                &         * R(ix-ixup, iy-iyup, it-itup, igork))
+            enddo
+          enddo
+        enddo
+      enddo
+    enddo
+
+  return
+  end subroutine DWilson
+
+  pure subroutine dslash_wilson(Phi,R,u,am,imass)
+    implicit none
+    !     calculates Phi = M*R
+    complex(dp), intent(in) :: u(0:ksizex_l+1, 0:ksizey_l+1, 0:ksizet_l+1, 3)
+    complex(dp), intent(out) :: Phi(kthird, 0:ksizex_l+1, 0:ksizey_l+1, 0:ksizet_l+1, 4)
+    complex(dp), intent(in) :: R(kthird, 0:ksizex_l+1, 0:ksizey_l+1, 0:ksizet_l+1, 4)
+    complex(dp) :: Rslice(0:ksizex_l+1, 0:ksizey_l+1, 0:ksizet_l+1, 4)
+    complex(dp) :: Phislice(0:ksizex_l+1, 0:ksizey_l+1, 0:ksizet_l+1, 4)
+    complex(dp) :: Mslice(0:ksizex_l+1, 0:ksizey_l+1, 0:ksizet_l+1, 4)
+    integer, intent(in) :: imass
+    real, intent(in) :: am
+    complex(dp) :: zkappa
+    real :: diag
+    integer :: ixup, iyup, itup, ix, iy, it, idirac, mu, igork
+    integer il
+
+    Phi=cmplx(0.0,0.0)
+
+    ! P+ component
+    Rslice=cmplx(0.0,0.0)
+    do il=1,kthird
+      Rslice(:,:,:,1:2)=R(il,:,:,:,1:2)
+      call DWilson(Phislice,Rslice,u,-am3)
+      ! diagonal
+      Phi(il,:,:,:,:)=Phi(il,:,:,:,:)+Phislice+Rslice
+      ! lower diagonal
+      if (il.lt.kthird) then
+        Phi(il+1,:,:,:,:)=Phi(il+1,:,:,:,:)+Phislice-Rslice
+      end if
+    end do
+
+    ! P- component
+    Rslice=cmplx(0.0,0.0)
+    do il=1,kthird
+      Rslice(:,:,:,3:4)=R(il,:,:,:,3:4)
+      call DWilson(Phislice,Rslice,u,-am3)
+      ! diagonal
+      Phi(il,:,:,:,:)=Phi(il,:,:,:,:)+Phislice+Rslice
+      ! upper diagonal
+      if (il.gt.1) then
+        Phi(il-1,:,:,:,:)=Phi(il-1,:,:,:,:)+Phislice-Rslice
+      end if
+    end do
+
+      !  Mass term - top right
+      Rslice=cmplx(0.0,0.0)
+      Rslice(:,:,:,1:2)=R(kthird,:,:,:,1:2)
+      call DWilson(Phislice,Rslice,u,-am3)
+      if (imass.eq.1) then
+        Mslice(:,:,:,:) = -am*(Phislice-Rslice)
+      elseif (imass.eq.3) then
+        Mslice(:,:,:,:) = -cmplx(0.0,am)*(Phislice-Rslice)
+      endif
+      Phi(1,1:ksizex_l,1:ksizey_l,1:ksizet_l,:) = &
+  &                Phi(1,1:ksizex_l,1:ksizey_l,1:ksizet_l,:)  &
+  & +             Mslice(1:ksizex_l,1:ksizey_l,1:ksizet_l,:)
+
+      !  Mass term - bottom left
+      Rslice=cmplx(0.0,0.0)
+      Rslice(:,:,:,3:4)=R(1,:,:,:,3:4)
+      call DWilson(Phislice,Rslice,u,-am3)
+      if (imass.eq.1) then
+        Mslice(:,:,:,:)=-am*(Phislice-Rslice)
+      elseif (imass.eq.3) then
+        Mslice(:,:,:,:)=cmplx(0.0,am)*(Phislice-Rslice)
+      endif
+      Phi(kthird,1:ksizex_l,1:ksizey_l,1:ksizet_l,:) = &
+  &                 Phi(kthird,1:ksizex_l,1:ksizey_l,1:ksizet_l,:) &
+  &            +        Mslice(1:ksizex_l,1:ksizey_l,1:ksizet_l,:)
+
+
+    !
+    return
+  end subroutine dslash_wilson
+
   !***********************************************************************
 
   pure subroutine dslashd_local_shamir(am, Phi, R, imass)
@@ -293,7 +414,8 @@ contains
     real, intent(in) :: am
     integer, dimension(16), intent(inout), optional :: reqs_R
 
-  call dslashd_shamir(Phi, R, u, am, imass, reqs_R)
+  ! call dslashd_shamir(Phi, R, u, am, imass, reqs_R)
+  call dslashd_wilson(Phi, R, u, am, imass, reqs_R)
 
 end subroutine dslashd
 
@@ -359,6 +481,137 @@ end subroutine dslashd
 
       return
   end subroutine dslashd_shamir
+
+  pure subroutine DWilsonD(Phi,R,u,mass)
+    implicit none
+    complex(dp), intent(out) :: Phi(0:ksizex_l+1, 0:ksizey_l+1, 0:ksizet_l+1, 4)
+    complex(dp), intent(in) :: R(0:ksizex_l+1, 0:ksizey_l+1, 0:ksizet_l+1, 4)
+    complex(dp), intent(in) :: u(0:ksizex_l+1, 0:ksizey_l+1, 0:ksizet_l+1, 3)
+    real, intent(in) :: mass
+    integer :: ixup, iyup, itup, ix, iy, it, idirac, mu, igork
+    real diag
+
+    diag=(3.0+mass)
+    Phi=diag*R
+    do mu=1,3
+      ixup = kdelta(1, mu)
+      iyup = kdelta(2, mu)
+      itup = kdelta(3, mu)
+
+      do idirac=1,4
+        igork=gamin(mu,idirac)
+        do it = 1,ksizet_l
+          do iy = 1,ksizey_l
+            do ix = 1,ksizex_l
+              phi(ix,iy,it,idirac)=phi(ix,iy,it,idirac) &
+              !   wilson term (hermitian)
+              &    - akappa * (u(ix,iy,it,mu) &
+              &              * r(ix+ixup, iy+iyup, it+itup, idirac) &
+              &             + conjg(u(ix-ixup, iy-iyup, it-itup, mu)) &
+              &              * r(ix-ixup, iy-iyup, it-itup, idirac)) &
+              !   dirac term (antihermitian)
+              &    - gamval(mu,idirac) * &
+              &       (u(ix,iy,it,mu) &
+              &         * r(ix+ixup, iy+iyup, it+itup, igork) &
+              &        - conjg(u(ix-ixup, iy-iyup, it-itup, mu)) &
+              &         * r(ix-ixup, iy-iyup, it-itup, igork))
+            enddo
+          enddo
+        enddo
+      enddo
+    enddo
+
+    return
+  end subroutine DWilsonD
+
+#ifdef MPI
+  subroutine dslashd_wilson(Phi, R, u, am, imass, reqs_R)
+#else
+  pure subroutine dslashd_wilson(Phi, R, u, am, imass, reqs_R)
+#endif
+    use comms, only : complete_halo_update
+    implicit none
+    complex(dp), intent(in) :: u(0:ksizex_l+1, 0:ksizey_l+1, 0:ksizet_l+1, 3)
+    complex(dp), intent(out) :: Phi(kthird, 0:ksizex_l+1, 0:ksizey_l+1, 0:ksizet_l+1, 4)
+    complex(dp), intent(in) :: R(kthird, 0:ksizex_l+1, 0:ksizey_l+1, 0:ksizet_l+1, 4)
+    complex(dp) :: Rslice(0:ksizex_l+1, 0:ksizey_l+1, 0:ksizet_l+1, 4)
+    complex(dp) :: Phislice(0:ksizex_l+1, 0:ksizey_l+1, 0:ksizet_l+1, 4)
+    complex(dp) :: Mslice(0:ksizex_l+1, 0:ksizey_l+1, 0:ksizet_l+1, 4)
+    integer, intent(in) :: imass
+    real, intent(in) :: am
+    integer :: ixup, iyup, itup, ix, iy, it, idirac, mu, igork
+    integer, dimension(16),intent(inout), optional :: reqs_R
+    integer il
+    complex(dp) :: zkappa
+    real :: diag
+
+#ifdef MPI
+      if (present(reqs_R)) then
+        call complete_halo_update(reqs_R)
+      end if
+#endif
+
+    Phi=cmplx(0.0,0.0)
+    do il=1,kthird
+      Rslice=R(il,:,:,:,:)
+      Phi(il,:,:,:,:)=Phi(il,:,:,:,:)+Rslice
+      ! lower diagonal
+      if (il.lt.kthird) then
+        Phi(il+1,:,:,:,3:4) = Phi(il+1,:,:,:,3:4)   &
+ &    - Rslice(:,:,:,3:4)
+      end if
+      ! upper diagonal
+      if (il.gt.1) then
+        Phi(il-1,:,:,:,1:2) = Phi(il-1,:,:,:,1:2)   &
+ &    - Rslice(:,:,:,1:2)
+      end if
+    enddo
+
+
+    do il=1,kthird
+      Rslice=R(il,:,:,:,:)
+      call DWilsonD(Phislice,Rslice,u,-am3)
+      Phi(il,:,:,:,:)=Phi(il,:,:,:,:)+Phislice
+      ! lower diagonal
+      if (il.lt.kthird) then
+        Phi(il+1,:,:,:,3:4) = Phi(il+1,:,:,:,3:4)   &
+ &    + Phislice(:,:,:,3:4)
+      end if
+      ! upper diagonal
+      if (il.gt.1) then
+        Phi(il-1,:,:,:,1:2) = Phi(il-1,:,:,:,1:2)   &
+ &    + Phislice(:,:,:,1:2)
+      end if
+    enddo
+
+      ! upper right mass term  
+      Rslice=R(kthird,:,:,:,:)
+      call DWilsonD(Phislice,Rslice,u,-am3)
+      Mslice=Phislice-Rslice
+      if (imass.eq.1) then
+        zkappa = -cmplx(am,0.0)
+      elseif (imass.eq.3) then
+        zkappa = -cmplx(0.0,am)
+      endif
+      Phi(1,1:ksizex_l,1:ksizey_l,1:ksizet_l,3:4) = &
+  &                Phi(1,1:ksizex_l,1:ksizey_l,1:ksizet_l,3:4)  &
+  & +      zkappa*Mslice(1:ksizex_l,1:ksizey_l,1:ksizet_l,3:4)
+
+      ! lower left mass term  
+      Rslice=R(1,:,:,:,:)
+      call DWilsonD(Phislice,Rslice,u,-am3)
+      Mslice=Phislice-Rslice
+      if (imass.eq.1) then
+        zkappa = -cmplx(am,0.0)
+      elseif (imass.eq.3) then
+        zkappa = cmplx(0.0,am)
+      endif
+      Phi(kthird,1:ksizex_l,1:ksizey_l,1:ksizet_l,1:2) = &
+  &                 Phi(kthird,1:ksizex_l,1:ksizey_l,1:ksizet_l,1:2) &
+  &            + zkappa*Mslice(1:ksizex_l,1:ksizey_l,1:ksizet_l,1:2)
+
+    return
+  end subroutine dslashd_wilson
 
   !***********************************************************************
   pure subroutine dslash2d(Phi, R, u)
