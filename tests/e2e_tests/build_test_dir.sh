54fae2c382a4be155f37374af8e53c155a06a80b
connoraird <connoraird@gmail.com>
2023-12-06 15:31:10 +0000

    Merge csd and myriad test scipts and remove submission line

--- tests/e2e_tests/build_test_dir.sh
+++ tests/e2e_tests/build_test_dir.sh
@@ -5,6 +5,7 @@ SAMPLES_DIR="samples"
 CONS_DIR="${SAMPLES_DIR}/cons"
 MIDOUT_FILE="${SAMPLES_DIR}/midout"
 MYRIAD_SUBMIT_FILE="myriad_submit.sh"
+CSD_SUBMIT_FILE="csd_submit.sh"
 
 # E2E test inputs
 KSIZE="4"
@@ -12,10 +13,9 @@ KSIZET="4"
 KTHIRD="8"
 ITER2="100"
 NP_THIRD="1"
-USE_MEASURE="yes"
-NP_X="${NP_X:-'4'}"
-NP_Y="${NP_Y:-'4'}"
-NP_T="${NP_T:-'2'}"
+NP_X="${NP_X:-4}"
+NP_Y="${NP_Y:-4}"
+NP_T="${NP_T:-2}"
 
 # Check we have a con file matching the inputs
 CON_FILE="${CONS_DIR}/con_${KSIZE}x${KSIZE}x${KSIZET}"
@@ -27,7 +27,7 @@ else
 fi
 
 # Create output folder
-OUTPUT_DIR="TEST_OUTPUT_${KSIZE}_${KSIZET}_${KTHIRD}_${ITER2}_${NP_X}_${NP_Y}_${NP_T}_${NP_THIRD}_${USE_MEASURE}"
+OUTPUT_DIR="TEST_OUTPUT_${KSIZE}_${KSIZET}_${KTHIRD}_${ITER2}_${NP_X}_${NP_Y}_${NP_T}_${NP_THIRD}"
 if [ -d "${OUTPUT_DIR}" ]; then
 	echo "Error: A submission for these inputs has already been made"
 	exit 0
@@ -44,9 +44,9 @@ echo "  NP_X:        ${NP_X}" >> "${OUTPUT_DIR}/inputs"
 echo "  NP_Y:        ${NP_Y}" >> "${OUTPUT_DIR}/inputs"
 echo "  NP_T:        ${NP_T}" >> "${OUTPUT_DIR}/inputs"
 echo "  NP_THIRD:    ${NP_THIRD}" >> "${OUTPUT_DIR}/inputs"
-echo "  USE_MEASURE: ${USE_MEASURE}" >> "${OUTPUT_DIR}/inputs"
 
-# Copy myriad_submit.sh, con, midout and remez* files from samples into output folder
+# Copy csd_submit.sh, con, midout and remez* files from samples into output folder
+cp $CSD_SUBMIT_FILE "./${OUTPUT_DIR}/${CSD_SUBMIT_FILE}"
 cp $MYRIAD_SUBMIT_FILE "./${OUTPUT_DIR}/${MYRIAD_SUBMIT_FILE}"
 cp $CON_FILE "./${OUTPUT_DIR}/con"
 cp $MIDOUT_FILE "./${OUTPUT_DIR}/midout"
@@ -58,15 +58,6 @@ sed -i "s/#define KSIZET .*/#define KSIZET ${KSIZET}/g" "${SOURCE_DIR}/params.F9
 sed -i "s/#define KTHIRD .*/#define KTHIRD ${KTHIRD}/g" "${SOURCE_DIR}/params.F90"
 sed -i "s/integer, parameter :: istart = .*/integer, parameter :: istart = -1/g" "${SOURCE_DIR}/params.F90"
 sed -i "s/integer, parameter :: iread = .*/integer, parameter :: iread = 1/g" "${SOURCE_DIR}/params.F90"
-
-# Switch between meson and measure
-if [ $USE_MEASURE -eq "yes" ]; then
-	sed -i "s/!call measure(pbp, respbp, ancgm, am, imass, isweep + isweep_total_start)/ call measure(pbp, respbp, ancgm, am, imass, isweep + isweep_total_start)/g" "${SOURCE_DIR}/dwf3d_lib.F90"
-	sed -i "s/ call meson(rescgm,itercg,ancgm,am,imass, isweep + isweep_total_start)/!call meson(rescgm,itercg,ancgm,am,imass, isweep + isweep_total_start)/g" "${SOURCE_DIR}/dwf3d_lib.F90"
-else
-	sed -i "s/ call measure(pbp, respbp, ancgm, am, imass, isweep + isweep_total_start)/!call measure(pbp, respbp, ancgm, am, imass, isweep + isweep_total_start)/g" "${SOURCE_DIR}/dwf3d_lib.F90"
-	sed -i "s/!call meson(rescgm,itercg,ancgm,am,imass, isweep + isweep_total_start)/ call meson(rescgm,itercg,ancgm,am,imass, isweep + isweep_total_start)/g" "${SOURCE_DIR}/dwf3d_lib.F90"
-fi
 
 # Update MkFlags with NP values 
 sed -i "s/NP_X=.*/NP_X=${NP_X}/g" "${SOURCE_DIR}/MkFlags"
@@ -86,13 +77,19 @@ cp "${SOURCE_DIR}/bulk_rhmc" "${OUTPUT_DIR}/bulk_rhmc"
 # replace iter2 in midout with user input
 sed -i "s/<ITER2>/${ITER2}/g" "${OUTPUT_DIR}/midout"
 
-# Update submission script with total NP
+# Update submissions scripts with total NP
 NP_TOTAL="$(($NP_X * $NP_Y * $NP_T * $NP_THIRD))"
 echo "NP_TOTAL: ${NP_TOTAL}"
 echo "Total number of processors: ${NP_TOTAL}"
-sed -i "s/#$ -pe mpi .*/#$ -pe mpi ${NP_TOTAL}/g" "${OUTPUT_DIR}/myriad_submit.sh"
-sed -i "s/<EXECUTABLE_PATH>/${OUTPUT_DIR}\/bulk_rhmc/g" "${OUTPUT_DIR}/myriad_submit.sh"
 
-# submit job
-cd $OUTPUT_DIR
-qsub myriad_submit.sh
+if [ $(($NP_TOTAL)) -gt 56 ]
+then
+   	echo "Warning: Cannot submit job with more than 56 processors to csd without manually updating nodes."
+else
+	sed -i "s/#SBATCH --ntasks=.*/#SBATCH --ntasks=${NP_TOTAL}/g" "${OUTPUT_DIR}/${CSD_SUBMIT_FILE}"
+fi
+
+sed -i "s/#$ -pe mpi .*/#$ -pe mpi ${NP_TOTAL}/g" "${OUTPUT_DIR}/myriad_submit.sh"
+
+sed -i "s/<EXECUTABLE_PATH>/${OUTPUT_DIR}\/bulk_rhmc/g" "${OUTPUT_DIR}/${CSD_SUBMIT_FILE}"
+sed -i "s/<EXECUTABLE_PATH>/${OUTPUT_DIR}\/bulk_rhmc/g" "${OUTPUT_DIR}/${MYRIAD_SUBMIT_FILE}"
