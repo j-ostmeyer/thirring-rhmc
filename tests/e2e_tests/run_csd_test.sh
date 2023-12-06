5fac34920548c2b4a28fceaed8ac6421ec4bb053
connoraird <connoraird@gmail.com>
2023-12-06 11:15:33 +0000

    Remove USE_MEASURE and update code to use measure instead of meson

--- tests/e2e_tests/run_csd_test.sh
+++ tests/e2e_tests/run_csd_test.sh
@@ -12,7 +12,6 @@ KSIZET="4"
 KTHIRD="8"
 ITER2="100"
 NP_THIRD="1"
-USE_MEASURE="1"
 NP_X="${NP_X:-4}"
 NP_Y="${NP_Y:-4}"
 NP_T="${NP_T:-2}"
@@ -27,7 +26,7 @@ else
 fi
 
 # Create output folder
-OUTPUT_DIR="TEST_OUTPUT_${KSIZE}_${KSIZET}_${KTHIRD}_${ITER2}_${NP_X}_${NP_Y}_${NP_T}_${NP_THIRD}_${USE_MEASURE}"
+OUTPUT_DIR="TEST_OUTPUT_${KSIZE}_${KSIZET}_${KTHIRD}_${ITER2}_${NP_X}_${NP_Y}_${NP_T}_${NP_THIRD}"
 if [ -d "${OUTPUT_DIR}" ]; then
 	echo "Error: A submission for these inputs has already been made"
 	exit 0
@@ -44,7 +43,6 @@ echo "  NP_X:        ${NP_X}" >> "${OUTPUT_DIR}/inputs"
 echo "  NP_Y:        ${NP_Y}" >> "${OUTPUT_DIR}/inputs"
 echo "  NP_T:        ${NP_T}" >> "${OUTPUT_DIR}/inputs"
 echo "  NP_THIRD:    ${NP_THIRD}" >> "${OUTPUT_DIR}/inputs"
-echo "  USE_MEASURE: ${USE_MEASURE}" >> "${OUTPUT_DIR}/inputs"
 
 # Copy csd_submit.sh, con, midout and remez* files from samples into output folder
 cp $SUBMIT_FILE "./${OUTPUT_DIR}/${SUBMIT_FILE}"
@@ -58,17 +56,6 @@ sed -i "s/#define KSIZET .*/#define KSIZET ${KSIZET}/g" "${SOURCE_DIR}/params.F9
 sed -i "s/#define KTHIRD .*/#define KTHIRD ${KTHIRD}/g" "${SOURCE_DIR}/params.F90"
 sed -i "s/integer, parameter :: istart = .*/integer, parameter :: istart = -1/g" "${SOURCE_DIR}/params.F90"
 sed -i "s/integer, parameter :: iread = .*/integer, parameter :: iread = 1/g" "${SOURCE_DIR}/params.F90"
-
-# Switch between meson and measure
-measure_line='call measure(pbp, respbp, ancgm, am, imass, isweep + isweep_total_start)'
-meson_line='call meson(rescgm,itercg,ancgm,am,imass, isweep + isweep_total_start)'
-if [ $(($USE_MEASURE)) -eq 1 ]; then
-	sed -E -i "s|^(\s*!*)\s*$measure_line|\1$measure_line|" "${SOURCE_DIR}/dwf3d_lib.F90"
-	sed -E -i "s|^(\s*)(!*)\s*$meson_line|\1!\2$meson_line|" "${SOURCE_DIR}/dwf3d_lib.F90"
-else
-	sed -E -i "s|^(\s*!*)\s*$meson_line|\1$meson_line|" "${SOURCE_DIR}/dwf3d_lib.F90"
-	sed -E -i "s|^(\s*)(!*)\s*$measure_line|\1!\2$measure_line|" "${SOURCE_DIR}/dwf3d_lib.F90"
-fi
 
 # Update MkFlags with NP values 
 sed -i "s/NP_X=.*/NP_X=${NP_X}/g" "${SOURCE_DIR}/MkFlags"
