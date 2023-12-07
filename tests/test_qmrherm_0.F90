bdc6c315343f7910b856b41c548e8c462a3c2a8a
connoraird <connoraird@gmail.com>
2023-12-07 10:02:43 +0000

    Do not generate X data in test_qmrherm_0

--- tests/test_qmrherm_0.F90
+++ tests/test_qmrherm_0.F90
@@ -3,6 +3,7 @@ program test_qmrherm_0
   ! use dwf3d_lib
   use trial, only: u
   ! use gdbhook
+  use vector, only: X
   use qmrherm_module, only: qmrherm, phi0, qmrhprint => printall
   !use dirac
   use gammamatrices
@@ -24,12 +25,12 @@ program test_qmrherm_0
   complex(dp) :: Phi(0:kthird_l + 1, 0:ksizex_l + 1, 0:ksizey_l + 1, 0:ksizet_l + 1, 4)
   complex(dp) :: Phi0_ref(kthird_l, ksizex_l, ksizey_l, ksizet_l, 4, 25)
   complex(dp) :: Phi0_orig(0:kthird_l + 1, 0:ksizex_l + 1, 0:ksizey_l + 1, 0:ksizet_l + 1, 4, 25)
-  complex(dp) :: X2_ref(kthird_l, ksizex_l, ksizey_l, ksizet_l, 4)
+  complex(dp) :: X_ref(kthird_l, ksizex_l, ksizey_l, ksizet_l, 4)
   complex(dp) :: delta_x(kthird_l, ksizex_l, ksizey_l, ksizet_l, 4)
   complex(dp) :: delta_Phi0(kthird_l, ksizex_l, ksizey_l, ksizet_l, 4, 25)
   complex(dp) :: R(0:kthird_l + 1, 0:ksizex_l + 1, 0:ksizey_l + 1, 0:ksizet_l + 1, 4)
   complex(dp) :: X2(0:kthird_l + 1, 0:ksizex_l + 1, 0:ksizey_l + 1, 0:ksizet_l + 1, 4)
-  real :: dSdpi_ref(ksizex_l, ksizey_l, ksizet_l, 3)
+  real(dp) :: dSdpi_ref(ksizex_l, ksizey_l, ksizet_l, 3)
 
   complex(dp) :: sum_delta_x, sum_delta_Phi0
   real(dp) :: max_delta_x, max_delta_Phi0
@@ -42,7 +43,7 @@ program test_qmrherm_0
   integer :: i
 
 #ifdef MPI
-  integer, dimension(16) :: reqs_R, reqs_Phi, reqs_X
+  integer, dimension(16) :: reqs_R, reqs_Phi, reqs_X2
   integer :: ierr
   call init_MPI
 #endif
@@ -59,34 +60,34 @@ program test_qmrherm_0
     aden(i) = real(exp(-iunit*0.5*i*tau/ndiag))
   enddo
 
-  ! dSdpi_ref is passed here simply to reduce the complexity of generate_starting_state
+  ! X2, reqs_X2 and dSdpi_ref are passed here simply to reduce the complexity of generate_starting_state
   call generate_starting_state(Phi, reqs_Phi, u, R, reqs_R, X2, reqs_X2, dSdpi_ref, Phi0_orig)
 
   ! call function
   do i = 1, timing_loops
     Phi0 = Phi0_orig
-    call qmrherm(Phi, X2, res, itercg, am, imass, anum, aden, ndiag, iflag)
+    call qmrherm(Phi, X, res, itercg, am, imass, anum, aden, ndiag, iflag)
   end do
 
   ! check output
   if (generate) then
 #ifdef MPI
-    write_file(X2(1:kthird_l, 1:ksizex_l, 1:ksizey_l, 1:ksizet_l, :), 'test_qmrherm_0_x.dat', MPI_Double_Complex)
+    write_file(X(1:kthird_l, 1:ksizex_l, 1:ksizey_l, 1:ksizet_l, :), 'test_qmrherm_0_x.dat', MPI_Double_Complex)
     write_file(Phi0(1:kthird_l, 1:ksizex_l, 1:ksizey_l, 1:ksizet_l, :, :), 'test_qmrherm_0_Phi0.dat', MPI_Double_Complex)
 #else
     write (6, *) "Generation not possible"
     call exit(1)
 #endif
   else
-    read_file(X2_ref, 'test_qmrherm_0_x.dat', MPI_Double_Complex)
+    read_file(X_ref, 'test_qmrherm_0_x.dat', MPI_Double_Complex)
     read_file(Phi0_ref, 'test_qmrherm_0_Phi0.dat', MPI_Double_Complex)
 
-    delta_x = X2(1:kthird_l, 1:ksizex_l, 1:ksizey_l, 1:ksizet_l, :) - X2_ref
+    delta_x = X(1:kthird_l, 1:ksizex_l, 1:ksizey_l, 1:ksizet_l, :) - X_ref
     delta_Phi0 = Phi0(1:kthird_l, 1:ksizex_l, 1:ksizey_l, 1:ksizet_l, :, :) - Phi0_ref
 
     check_equality(itercg, 2, 'itercg', 'test_qmrherm_0')
-    check_sum(delta_x, 50, 'X2', sum_delta_x, MPI_Double_Complex, 'test_qmrherm_0')
-    check_max(delta_x, 0.5, 'X2', max_delta_x, MPI_Double_Precision, 'test_qmrherm_0')
+    check_sum(delta_x, 50, 'X', sum_delta_x, MPI_Double_Complex, 'test_qmrherm_0')
+    check_max(delta_x, 0.5, 'X', max_delta_x, MPI_Double_Precision, 'test_qmrherm_0')
     check_sum(delta_Phi0, 6e-12, 'Phi0', sum_delta_Phi0, MPI_Double_Complex, 'test_qmrherm_0')
     check_max(delta_Phi0, 1e-14, 'Phi0', max_delta_Phi0, MPI_Double_Precision, 'test_qmrherm_0')
   end if
